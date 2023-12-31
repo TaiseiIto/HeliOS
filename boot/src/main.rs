c46f983e5ccb33c20ad1d694f5bd928ae21a4fb4
TaiseiIto <taisei1212@outlook.jp>
2023-12-31 18:51:34 +0900

    Build a bootloader #3

--- boot/src/main.rs
+++ boot/src/main.rs
@@ -1,19 +1,19 @@
 #![no_main]
 #![no_std]
-#![feature(abi_efiabi)]
+#![feature(abi_efiapi)]
 #![allow(stable_features)]
 
-use core::arch::asm;
+use core::panic::PanicInfo;
 
 mod asm;
 
-#![no_mangle]
+#[no_mangle]
 fn efi_main() {
     panic!("Hello, World!");
 }
 
-#![panic_handler]
-fn panic(panic: &PanicInfo) -> ! {
+#[panic_handler]
+fn panic(_panic: &PanicInfo) -> ! {
     loop {
         asm::hlt();
     }
