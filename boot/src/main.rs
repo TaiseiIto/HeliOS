9db4de73d9bc33383422b6ea8cab5a617b1508c8
TaiseiIto <taisei1212@outlook.jp>
2024-01-03 12:05:54 +0900

    Incomplete bootloader returns EFI_ABORTED #6

--- boot/src/main.rs
+++ boot/src/main.rs
@@ -11,17 +11,17 @@ mod asm;
 mod efi;
 mod rs232c;
 
-/// # Entry point of the OS
+/// # The entry point of the OS
 /// ## References
 /// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 4.1 UEFI Image Entry Point
 #[no_mangle]
-fn efi_main(image_handle: usize, system_table: usize) {
+fn efi_main(image_handle: usize, system_table: usize) -> efi::Status {
     com2_println!("image_handle = {:#x?}", image_handle);
     com2_println!("system_table = {:#x?}", system_table);
-    panic!("Hello, World!");
+    efi::ABORTED
 }
 
-/// # Boot panic
+/// # A panic handler of the boot loader
 #[panic_handler]
 fn panic(panic: &PanicInfo) -> ! {
     com2_println!("BOOT PANIC!!!");
