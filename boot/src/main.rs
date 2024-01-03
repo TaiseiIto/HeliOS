4d2feb39c1ecde7a47c139f2548bdd0a1eb28fb1
TaiseiIto <taisei1212@outlook.jp>
2024-01-03 11:28:35 +0900

    Get an UEFI system table #6

--- boot/src/main.rs
+++ boot/src/main.rs
@@ -8,11 +8,16 @@
 use core::panic::PanicInfo;
 
 mod asm;
+mod efi;
 mod rs232c;
 
 /// # Entry point of the OS
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 4.1 UEFI Image Entry Point
 #[no_mangle]
-fn efi_main() {
+fn efi_main(image_handle: usize, system_table: usize) {
+    com2_println!("image_handle = {:#x?}", image_handle);
+    com2_println!("system_table = {:#x?}", system_table);
     panic!("Hello, World!");
 }
 
