b1c7b162e3157174a3ece9142141efb2ea1f54ad
TaiseiIto <taisei1212@outlook.jp>
2024-01-04 14:05:11 +0900

    Read EFI_SYSTEM_TABLE #6

--- boot/src/efi/boot_services.rs
+++ boot/src/efi/boot_services.rs
@@ -5,5 +5,22 @@
 #[repr(C)]
 pub struct BootServices {
     hdr: super::TableHeader,
+    raise_tpl: RaiseTpl,
+    restore_tpl: RestoreTpl,
 }
+
+/// # EFI_RAISE_TPL
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.1 Event, Timer, and Task Priority Services
+type RaiseTpl = extern "efiapi" fn(Tpl) -> Tpl;
+
+/// # EFI_RESTORE_TPL
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.1 Event, Timer, and Task Priority Services
+type RestoreTpl = extern "efiapi" fn(Tpl);
+
+/// # EFI_TPL
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.1 Event, Timer, and Task Priority Services
+type Tpl = usize;
 
