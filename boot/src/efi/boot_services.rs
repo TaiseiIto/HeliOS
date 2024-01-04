d6af16f15894a002de4a0a61542087560baca6dc
TaiseiIto <taisei1212@outlook.jp>
2024-01-04 21:16:26 +0900

    Read EFI_SYSTEM_TABLE #6

--- boot/src/efi/boot_services.rs
+++ boot/src/efi/boot_services.rs
@@ -49,17 +49,36 @@ pub struct BootServices<'a> {
     get_next_monotonic_count: GetNextMonotonicCount,
     stall: Stall,
     set_watchdog_timer: SetWatchdogTimer,
+    connect_controller: ConnectController,
+    disconnect_controller: DisconnectController,
+    open_protocol: OpenProtocol,
+    close_protocol: CloseProtocol,
+    open_protocol_information: OpenProtocolInformation,
+    protocols_per_handle: ProtocolsPerHandle,
+    locate_handle_buffer: LocateHandleBuffer,
+    locate_protocol: LocateProtocol,
+    install_multiple_protocol_interfaces: InstallMultipleProtocolInterfaces,
+    uninstall_multiple_protocol_interfaces: UninstallMultipleProtocolInterfaces,
+    calculate_crc32: CalculateCrc32,
+    copy_mem: CopyMem,
+    set_mem: SetMem,
+    create_event_ex: CreateEventEx,
 }
+
+/// # EFI_CREATE_EVENT
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.1 Event, Timer, and Task Priority Services
+type CreateEvent = extern "efiapi" fn(/* Type */ u32, /* NotifyTpl */ Tpl, /* NotifyFunction */ event::Notify, /* NotifyContext */ &Void, /* Event */ &mut Event) -> Status;
 
 /// # EFI_TPL
 /// ## References
 /// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.1 Event, Timer, and Task Priority Services
 type Tpl = usize;
 
-/// # EFI_CREATE_EVENT
+/// # EFI_CREATE_EVENT_EX
 /// ## References
 /// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.1 Event, Timer, and Task Priority Services
-type CreateEvent = extern "efiapi" fn(/* Type */ u32, /* NotifyTpl */ Tpl, /* NotifyFunction */ event::Notify, /* NotifyContext */ &Void, /* event */ &mut Event) -> Status;
+type CreateEventEx = extern "efiapi" fn(/* Type */ u32, /* NotifyTpl */ Tpl, /* NotifyFunction */ event::Notify, /* NotifyContext */ &Void, /* EventGroup */ &Guid, /* Event */ &mut Event) -> Status;
 
 /// # EFI_CLOSE_EVENT
 /// ## References
@@ -124,16 +143,7 @@ type FreePool = extern "efiapi" fn(/* Buffer */ &Void) -> Status;
 /// # EFI_INSTALL_PROTOCOL_INTERFACE
 /// ## References
 /// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.3 Protocol Handler Services
-type InstallProtocolInterface = extern "efiapi" fn(/* Handle */ &mut Handle, /* Protocol */ &Guid, /* InterfaceType */ InterfaceType, /* Interface */ &Void) -> Status;
-
-/// # EFI_INTERFACE_TYPE
-/// ## References
-/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.3 Protocol Handler Services
-#[derive(Debug)]
-#[repr(C)]
-pub enum InterfaceType {
-    NativeInterface,
-}
+type InstallProtocolInterface = extern "efiapi" fn(/* Handle */ &mut Handle, /* Protocol */ &Guid, /* InterfaceType */ protocol::InterfaceType, /* Interface */ &Void) -> Status;
 
 /// # EFI_UNINSTALL_PROTOCOL_INTERFACE
 /// ## References
@@ -153,18 +163,7 @@ type RegisterProtocolNotify = extern "efiapi" fn(/* Protocol */ &Guid, /* Event 
 /// # EFI_LOCATE_HANDLE
 /// ## References
 /// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.3 Protocol Handler Services
-type LocateHandle = extern "efiapi" fn(/* SearchType */ LocateSearchType, /* Protocol */ &Guid, /* SearchKey */ &Void, /* BufferSize */ &mut usize, /* Buffer */ &mut Handle) -> Status;
-
-/// # EFI_LOCATE_SEARCH_TYPE
-/// ## References
-/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.3 Protocol Handler Services
-#[derive(Debug)]
-#[repr(C)]
-pub enum LocateSearchType {
-    AllHandles,
-    ByRegisterNotify,
-    ByProtocol,
-}
+type LocateHandle = extern "efiapi" fn(/* SearchType */ protocol::LocateSearchType, /* Protocol */ &Guid, /* SearchKey */ &Void, /* BufferSize */ &mut usize, /* Buffer */ &mut Handle) -> Status;
 
 /// # EFI_HANDLE_HANDLE
 /// ## References
@@ -175,6 +174,56 @@ type HandleProtocol = extern "efiapi" fn(/* Handle */ Handle, /* Protocol */ &Gu
 /// ## References
 /// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.3 Protocol Handler Services
 type LocateDevicePath = extern "efiapi" fn(/* Protocol */ &Guid, /* DevicePath */ &mut &protocol::DevicePath, /* Device */ &mut Handle) -> Status;
+
+/// # EFI_OPEN_PROTOCOL
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.3 Protocol Handler Services
+type OpenProtocol = extern "efiapi" fn(/* Handle */ Handle, /* Protocol */ &Guid, /* Interface */ &mut &Void, /* AgentHandle */ Handle, /* ControllerHandle */ Handle, /* Attributes */ u32) -> Status;
+
+/// # EFI_CLOSE_PROTOCOL
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.3 Protocol Handler Services
+type CloseProtocol = extern "efiapi" fn(/* Handle */ Handle, /* Protocol */ &Guid, /* AgentHandle */ Handle, /* ControllerHandle */ Handle) -> Status;
+
+/// # EFI_OPEN_PROTOCOL_INFORMATION
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.3 Protocol Handler Services
+type OpenProtocolInformation = extern "efiapi" fn(/* Handle */ Handle, /* Protocol */ &Guid, /* EntryBuffer */ &mut &protocol::OpenProtocolInformationEntry, /* EntryCount */ &mut usize) -> Status;
+
+/// # EFI_CONNECT_CONTROLLER
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.3 Protocol Handler Services
+type ConnectController = extern "efiapi" fn(/* ControllerHandle */ Handle, /* DriverImageHandle */ &Handle, /* RemainingDevicePath */ &protocol::DevicePath, /* Recursive */ bool) -> Status;
+
+/// # EFI_DISCONNECT_CONTROLLER
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.3 Protocol Handler Services
+type DisconnectController = extern "efiapi" fn(/* ControllerHandle */ Handle, /* DriverImageHandle */ Handle, /* ChildHandle */ Handle) -> Status;
+
+/// # EFI_PROTOCOLS_PER_HANDLE
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.3 Protocol Handler Services
+type ProtocolsPerHandle = extern "efiapi" fn(/* Handle */ Handle, /* ProtocolBuffer */ &mut &&Guid, /* ProtocolBufferCount */ &mut usize) -> Status;
+
+/// # EFI_LOCATE_HANDLE_BUFFER
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.3 Protocol Handler Services
+type LocateHandleBuffer = extern "efiapi" fn(/* SearchType */ protocol::LocateSearchType, /* Protocol */ &Guid, /* SearchKey */ &Void, /* NoHandles */ &mut usize, /* Buffer */ &mut &Handle) -> Status;
+
+/// # EFI_LOCATE_PROTOCOL
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.3 Protocol Handler Services
+type LocateProtocol = extern "efiapi" fn(/* Protocl */ &Guid, /* Registration */ &Void, /* Interface */&mut &Void) -> Status;
+
+/// # EFI_INSTALL_MULTIPLE_PROTOCOL_INTERFACES
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.3 Protocol Handler Services
+type InstallMultipleProtocolInterfaces = extern "efiapi" fn(/* Handle */ &mut Handle) -> Status;
+
+/// # EFI_UNINSTALL_MULTIPLE_PROTOCOL_INTERFACES
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.3 Protocol Handler Services
+type UninstallMultipleProtocolInterfaces = extern "efiapi" fn(/* Handle */ &mut Handle) -> Status;
 
 /// # EFI_IMAGE_LOAD
 /// ## References
@@ -206,6 +255,16 @@ type ExitBootServices = extern "efiapi" fn(/* ImageHandle */ Handle, /* MapKey *
 /// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.5 Miscellaneous Boot Services
 type SetWatchdogTimer = extern "efiapi" fn(/* Timeout */ usize, /* WatchdogCode */ u64, /* DataSize */ usize, /* WatchdogData */ char16::NullTerminatedString) -> Status;
 
+/// # EFI_COPY_MEM
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.5 Miscellaneous Boot Services
+type CopyMem = extern "efiapi" fn(/* Destination */ &Void, /* Source */ &Void, /* Length */ usize) -> Status;
+
+/// # EFI_SET_MEM
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.5 Miscellaneous Boot Services
+type SetMem = extern "efiapi" fn(/* Buffer */ &Void, /* Size */ usize, /* Value */ u8) -> Status;
+
 /// # EFI_INSTALL_CONFIGURATION_TABLE
 /// ## References
 /// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.5 Miscellaneous Boot Services
@@ -220,4 +279,9 @@ type Stall = extern "efiapi" fn(/* Microseconds */ usize) -> Status;
 /// ## References
 /// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.5 Miscellaneous Boot Services
 type GetNextMonotonicCount = extern "efiapi" fn(/* Count */ &mut u64) -> Status;
+
+/// # EFI_CALCULATE_CRC32
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.5 Miscellaneous Boot Services
+type CalculateCrc32 = extern "efiapi" fn(/* Data */ &Void, /* DataSize */ usize, /* Crc32 */&mut u32) -> Status;
 
--- boot/src/efi/protocol.rs
+++ boot/src/efi/protocol.rs
@@ -1,3 +1,37 @@
+use super::Handle;
+
+/// # EFI_INTERFACE_TYPE
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.3 Protocol Handler Services
+#[derive(Debug)]
+#[repr(C)]
+pub enum InterfaceType {
+    NativeInterface,
+}
+
+/// # EFI_LOCATE_SEARCH_TYPE
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.3 Protocol Handler Services
+#[derive(Debug)]
+#[repr(C)]
+pub enum LocateSearchType {
+    AllHandles,
+    ByRegisterNotify,
+    ByProtocol,
+}
+
+/// # EFI_OPEN_PROTOCOL_INFORMATION
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 7.3 Protocol Handler Services
+#[derive(Debug)]
+#[repr(C)]
+pub struct OpenProtocolInformationEntry<'a> {
+    agent_handle: Handle<'a>,
+    controller_handle: Handle<'a>,
+    attributes: u32,
+    open_count: u32,
+}
+
 /// # EFI_DEVICE_PATH_PROTOCOL
 /// ## References
 /// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 10.2 EFI Device Path Protocol
