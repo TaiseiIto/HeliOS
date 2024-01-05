d5d34341a45691a38b94df4b8c5d13b252f0926e
TaiseiIto <taisei1212@outlook.jp>
2024-01-05 10:13:13 +0900

    A global UEFI system table #6

--- boot/src/efi/system_table.rs
+++ boot/src/efi/system_table.rs
@@ -9,6 +9,8 @@ use super::{
     configuration,
 };
 
+static mut SYSTEM_TABLE: Option<&'static SystemTable<'static>> = None;
+
 /// # EFI_SYSTEM_TABLE
 /// ## References
 /// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 4.3 EFI System Table
@@ -32,6 +34,20 @@ pub struct SystemTable<'a> {
 impl SystemTable<'_> {
     pub fn shutdown(&self) {
         self.runtime_services.shutdown();
+    }
+}
+
+impl SystemTable<'static> {
+    pub fn get() -> &'static Self {
+        unsafe {
+            SYSTEM_TABLE.expect("Can't get a UEFI system table!")
+        }
+    }
+
+    pub fn set(&'static self) {
+        unsafe {
+            SYSTEM_TABLE = Some(self);
+        }
     }
 }
 
