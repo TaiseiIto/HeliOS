a13b4547f1734aa8bf7b9374d6d649887970db22
TaiseiIto <taisei1212@outlook.jp>
2024-01-07 14:16:21 +0900

    Read MSR #9

--- boot/src/asm.rs
+++ boot/src/asm.rs
@@ -43,3 +43,20 @@ pub fn outb(port: u16, data: u8) {
     }
 }
 
+/// # Read From Model Specific Register
+/// ## References
+/// * [Intel 64 and IA-32 Architectures Software Developer's Manual December 2023](https://www.intel.com/content/www/us/en/developer/articles/technical/intel-sdm.html) Vol.2B 4-4-537
+pub fn rdmsr(ecx: u32) -> u64 {
+    let mut eax: u32;
+    let mut edx: u32;
+    unsafe {
+        asm!(
+            "rdmsr",
+            in("ecx") ecx,
+            out("eax") eax,
+            out("edx") edx,
+        );
+    }
+    (eax as u64) + ((edx as u64) << u32::BITS)
+}
+
