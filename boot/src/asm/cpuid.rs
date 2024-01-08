1419162201edbdd9b428cd607e378cb38b6aa8d5
TaiseiIto <taisei1212@outlook.jp>
2024-01-08 20:48:42 +0900

    Read CPUID #9

--- boot/src/asm/cpuid.rs
+++ boot/src/asm/cpuid.rs
@@ -9,12 +9,14 @@ mod eax0x00000001;
 mod eax0x00000002;
 mod eax0x00000003;
 mod eax0x00000004;
+mod eax0x00000005;
 
 pub use eax0x00000000::Eax0x00000000;
 pub use eax0x00000001::Eax0x00000001;
 pub use eax0x00000002::Eax0x00000002;
 pub use eax0x00000003::Eax0x00000003;
 pub use eax0x00000004::Eax0x00000004;
+pub use eax0x00000005::Eax0x00000005;
 
 /// # CPUID
 /// ## References
@@ -26,6 +28,7 @@ pub struct Cpuid {
     eax0x00000002: Option<Eax0x00000002>,
     eax0x00000003: Option<Eax0x00000003>,
     eax0x00000004: Option<Eax0x00000004>,
+    eax0x00000005: Option<Eax0x00000005>,
 }
 
 impl Cpuid {
@@ -39,12 +42,14 @@ impl Cpuid {
                 .map(|eax0x00000001| Eax0x00000003::get(&eax0x00000000, &eax0x00000001))
                 .flatten();
             let eax0x00000004: Option<Eax0x00000004> = Eax0x00000004::get(&eax0x00000000);
+            let eax0x00000005: Option<Eax0x00000005> = Eax0x00000005::get(&eax0x00000000);
             Some(Self {
                 eax0x00000000,
                 eax0x00000001,
                 eax0x00000002,
                 eax0x00000003,
                 eax0x00000004,
+                eax0x00000005,
             })
         } else {
             None
---
+++ boot/src/asm/cpuid/eax0x00000005.rs
@@ -0,0 +1,82 @@
+//! # CPUID
+//! ## References
+//! * [Intel 64 and IA-32 Architectures Software Developer's Manual December 2023](https://www.intel.com/content/www/us/en/developer/articles/technical/intel-sdm.html) Vol. 2A 3-217
+
+use {
+    bitfield_struct::bitfield,
+    super::{
+        Eax0x00000000,
+        Return,
+    },
+};
+
+#[derive(Debug)]
+pub struct Eax0x00000005 {
+    eax: Eax,
+    ebx: Ebx,
+    ecx: Ecx,
+    edx: Edx,
+}
+
+impl Eax0x00000005 {
+    pub fn get(eax0x00000000: &Eax0x00000000) -> Option<Self> {
+        let eax: u32 = 0x00000005;
+        let ecx: u32 = 0x00000000;
+        if eax <= eax0x00000000.max_eax() {
+            let eax0x00000001 = Return::get(eax, ecx);
+            let eax: Eax = eax0x00000001.eax().into();
+            let ebx: Ebx = eax0x00000001.ebx().into();
+            let ecx: Ecx = eax0x00000001.ecx().into();
+            let edx: Edx = eax0x00000001.edx().into();
+            Some(Self {
+                eax,
+                ebx,
+                ecx,
+                edx,
+            })
+        } else {
+            None
+        }
+    }
+}
+
+#[bitfield(u32)]
+pub struct Eax {
+    smallest_monitor_line_size_in_bytes: u16,
+    reserved0: u16,
+}
+
+#[bitfield(u32)]
+pub struct Ebx {
+    largest_monitor_line_size_in_bytes: u16,
+    reserved0: u16,
+}
+
+#[bitfield(u32)]
+pub struct Ecx {
+    enumeration_of_monitor_mwait_extensions_beyond_eax_and_ebx_registers_supported: bool,
+    supports_treating_interrupts_as_break_event_for_mwait_even_when_interrupts_disabled: bool,
+    #[bits(30)]
+    reserved0: u32,
+}
+
+#[bitfield(u32)]
+pub struct Edx  {
+    #[bits(4)]
+    number_of_c0_sub_c_states_supported_using_mwait: u8,
+    #[bits(4)]
+    number_of_c1_sub_c_states_supported_using_mwait: u8,
+    #[bits(4)]
+    number_of_c2_sub_c_states_supported_using_mwait: u8,
+    #[bits(4)]
+    number_of_c3_sub_c_states_supported_using_mwait: u8,
+    #[bits(4)]
+    number_of_c4_sub_c_states_supported_using_mwait: u8,
+    #[bits(4)]
+    number_of_c5_sub_c_states_supported_using_mwait: u8,
+    #[bits(4)]
+    number_of_c6_sub_c_states_supported_using_mwait: u8,
+    #[bits(4)]
+    number_of_c7_sub_c_states_supported_using_mwait: u8,
+}
+
