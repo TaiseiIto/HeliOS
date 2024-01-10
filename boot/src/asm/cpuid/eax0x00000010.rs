b8af93e3cfd74221c52009a06f869dea72390181
TaiseiIto <taisei1212@outlook.jp>
2024-01-10 11:42:40 +0900

    Read CPUID #9

--- boot/src/asm/cpuid/eax0x00000010.rs
+++ boot/src/asm/cpuid/eax0x00000010.rs
@@ -2,43 +2,31 @@
 //! ## References
 //! * [Intel 64 and IA-32 Architectures Software Developer's Manual December 2023](https://www.intel.com/content/www/us/en/developer/articles/technical/intel-sdm.html) Vol. 2A 3-217
 
-use {
-    bitfield_struct::bitfield,
-    super::{
-        Eax0x00000000,
-        Return,
-    },
+mod ecx0x00000000;
+
+use super::Eax0x00000000;
+
+pub use {
+    ecx0x00000000::Ecx0x00000000,
 };
 
 #[derive(Debug)]
 pub struct Eax0x00000010 {
     #[allow(dead_code)]
-    ebx: Ebx,
+    ecx0x00000000: Ecx0x00000000,
 }
 
 impl Eax0x00000010 {
     pub fn get(eax0x00000000: &Eax0x00000000) -> Option<Self> {
         let eax: u32 = 0x00000010;
-        let ecx: u32 = 0x00000000;
         if eax <= eax0x00000000.max_eax() {
-            let eax0x0000000f = Return::get(eax, ecx);
-            let ebx: Ebx = eax0x0000000f.ebx().into();
+            let ecx0x00000000 = Ecx0x00000000::get(eax);
             Some(Self {
-                ebx,
+                ecx0x00000000,
             })
         } else {
             None
         }
     }
-}
-
-#[bitfield(u32)]
-pub struct Ebx {
-    reserved0: bool,
-    supports_l3_cache_allocation_technology: bool,
-    supports_l2_cache_allocation_technology: bool,
-    supports_memory_bandwidth_allocation: bool,
-    #[bits(28, access = RO)]
-    reserved1: u32,
 }
 
---
+++ boot/src/asm/cpuid/eax0x00000010/ecx0x00000000.rs
@@ -0,0 +1,36 @@
+//! # CPUID
+//! ## References
+//! * [Intel 64 and IA-32 Architectures Software Developer's Manual December 2023](https://www.intel.com/content/www/us/en/developer/articles/technical/intel-sdm.html) Vol. 2A 3-217
+
+use {
+    bitfield_struct::bitfield,
+    super::super::Return,
+};
+
+#[derive(Debug)]
+pub struct Ecx0x00000000 {
+    #[allow(dead_code)]
+    ebx: Ebx,
+}
+
+impl Ecx0x00000000 {
+    pub fn get(eax: u32) -> Self {
+        let ecx: u32 = 0x00000000;
+        let ecx0x00000000 = Return::get(eax, ecx);
+        let ebx: Ebx = ecx0x00000000.ebx().into();
+        Self {
+            ebx,
+        }
+    }
+}
+
+#[bitfield(u32)]
+pub struct Ebx {
+    reserved0: bool,
+    supports_l3_cache_allocation_technology: bool,
+    supports_l2_cache_allocation_technology: bool,
+    supports_memory_bandwidth_allocation: bool,
+    #[bits(28, access = RO)]
+    reserved1: u32,
+}
+
