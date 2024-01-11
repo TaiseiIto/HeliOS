3c257a532098aca0fae0687e23eb248d96f96e34
TaiseiIto <taisei1212@outlook.jp>
2024-01-11 20:16:31 +0900

    Read CPUID #9

---
+++ boot/src/asm/cpuid/eax0x0000001d/ecx0x00000001.rs
@@ -0,0 +1,53 @@
+//! # CPUID
+//! ## References
+//! * [Intel 64 and IA-32 Architectures Software Developer's Manual December 2023](https://www.intel.com/content/www/us/en/developer/articles/technical/intel-sdm.html) Vol. 2A 3-217
+
+use {
+    bitfield_struct::bitfield,
+    super::super::Return,
+};
+
+#[derive(Debug)]
+pub struct Ecx0x00000001 {
+    #[allow(dead_code)]
+    eax: Eax,
+    #[allow(dead_code)]
+    ebx: Ebx,
+    #[allow(dead_code)]
+    ecx: Ecx,
+}
+
+impl Ecx0x00000001 {
+    pub fn get(eax: u32) -> Self {
+        let ecx: u32 = 0x00000000;
+        let ecx0x00000001 = Return::get(eax, ecx);
+        let eax: Eax = ecx0x00000001.eax().into();
+        let ebx: Ebx = ecx0x00000001.ebx().into();
+        let ecx: Ecx = ecx0x00000001.ecx().into();
+        Self {
+            eax,
+            ebx,
+            ecx,
+        }
+    }
+}
+
+#[bitfield(u32)]
+struct Eax {
+    palette_1_total_tile_bytes: u16,
+    palette_1_bytes_per_tile: u16,
+}
+
+#[bitfield(u32)]
+struct Ebx {
+    palette_1_bytes_per_row: u16,
+    palette_1_max_names: u16,
+}
+
+#[bitfield(u32)]
+struct Ecx {
+    palette_1_max_rows: u16,
+    #[bits(access = RO)]
+    reserved0: u16,
+}
+
