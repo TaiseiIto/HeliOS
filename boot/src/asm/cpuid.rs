bb58516109fb16157d8d5fc869d7cdd7c30296f3
TaiseiIto <taisei1212@outlook.jp>
2024-01-11 20:51:23 +0900

    Read CPUID #9

--- boot/src/asm/cpuid.rs
+++ boot/src/asm/cpuid.rs
@@ -34,6 +34,7 @@ mod eax0x0000001c;
 mod eax0x0000001d;
 mod eax0x0000001e;
 mod eax0x0000001f;
+mod eax0x00000020;
 
 pub use {
     eax0x00000000::Eax0x00000000,
@@ -63,6 +64,7 @@ pub use {
     eax0x0000001d::Eax0x0000001d,
     eax0x0000001e::Eax0x0000001e,
     eax0x0000001f::Eax0x0000001f,
+    eax0x00000020::Eax0x00000020,
 };
 
 /// # CPUID
@@ -124,6 +126,8 @@ pub struct Cpuid {
     eax0x0000001e: Option<Eax0x0000001e>,
     #[allow(dead_code)]
     eax0x0000001f: Option<Eax0x0000001f>,
+    #[allow(dead_code)]
+    eax0x00000020: Option<Eax0x00000020>,
 }
 
 impl Cpuid {
@@ -158,6 +162,7 @@ impl Cpuid {
             let eax0x0000001d: Option<Eax0x0000001d> = Eax0x0000001d::get(&eax0x00000000);
             let eax0x0000001e: Option<Eax0x0000001e> = Eax0x0000001e::get(&eax0x00000000);
             let eax0x0000001f: Option<Eax0x0000001f> = Eax0x0000001f::get(&eax0x00000000);
+            let eax0x00000020: Option<Eax0x00000020> = Eax0x00000020::get(&eax0x00000000);
             Some(Self {
                 eax0x00000000,
                 eax0x00000001,
@@ -186,6 +191,7 @@ impl Cpuid {
                 eax0x0000001d,
                 eax0x0000001e,
                 eax0x0000001f,
+                eax0x00000020,
             })
         } else {
             None
