4477906e189087365038bb0ab994934cd245737c
TaiseiIto <taisei1212@outlook.jp>
2024-01-13 15:31:58 +0900

    Read a paging structure #9

--- boot/src/memory/paging/level4.rs
+++ boot/src/memory/paging/level4.rs
@@ -32,7 +32,7 @@ impl From<asm::control::Register3> for Pml4t<'static> {
         let vaddr2pdpt: BTreeMap<usize, Pdpt<'_>> = pml4t
             .iter_mut()
             .enumerate()
-            .map(|(index, pml4e)| (index, pml4e.into()))
+            .map(|(index, pml4e)| (Vaddr::create(index, 0, 0, 0, 0).into(), pml4e.into()))
             .collect();
         Self {
             cr3,
@@ -75,6 +75,51 @@ impl<'a> From<&'a mut Pml4e> for Pdpt<'a> {
         Self {
             pml4e,
         }
+    }
+}
+
+/// # 4-Level Paging PML4E
+/// ## References
+/// * [Intel 64 and IA-32 Architectures Software Developer's Manual December 2023](https://www.intel.com/content/www/us/en/developer/articles/technical/intel-sdm.html) Vol.3A 4-22 Figure 4-8. Linear-Address Translation to a 4-KByte Page Using 4-Level Paging
+#[bitfield(u64)]
+struct Vaddr {
+    #[bits(12)]
+    offset: u16,
+    #[bits(9)]
+    p: u16,
+    #[bits(9)]
+    pd: u16,
+    #[bits(9)]
+    pdp: u16,
+    #[bits(9)]
+    pml4: u16,
+    reserved0: u16,
+}
+
+impl Vaddr {
+    fn create(pml4: usize, pdp: usize, pd: usize, p: usize, offset: usize) -> Self {
+        let pml4: u16 = pml4 as u16;
+        let pdp: u16 = pdp as u16;
+        let pd: u16 = pd as u16;
+        let p: u16 = p as u16;
+        let offset: u16 = offset as u16;
+        Self::new()
+            .with_offset(offset)
+            .with_p(p)
+            .with_pd(pd)
+            .with_pdp(pdp)
+            .with_pml4(pml4)
+            .with_reserved0(match pml4 & (1 << (Self::PML4_BITS - 1)) {
+                0 => 0,
+                _ => 0xffff,
+            })
+    }
+}
+
+impl From<Vaddr> for usize {
+    fn from(vaddr: Vaddr) -> Self {
+        let vaddr: u64 = vaddr.into();
+        vaddr as Self
     }
 }
 
