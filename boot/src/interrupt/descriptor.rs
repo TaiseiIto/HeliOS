007f1bf8911e04dec0d7523c9ff58e25959c07e7
TaiseiIto <taisei1212@outlook.jp>
2024-01-14 21:09:45 +0900

    Read IDT #9

--- boot/src/interrupt/descriptor.rs
+++ boot/src/interrupt/descriptor.rs
@@ -2,14 +2,17 @@ pub mod table;
 
 pub use table::Table;
 
-use bitfield_struct::bitfield;
+use {
+    bitfield_struct::bitfield,
+    crate::x64::descriptor::Type,
+};
 
 /// # Interrupt Descriptor
 /// ## References
 /// * [Intel 64 and IA-32 Architectures Software Developer's Manual December 2023](https://www.intel.com/content/www/us/en/developer/articles/technical/intel-sdm.html) Vol.3A 6.14.1 64-Bit Mode IDT, Figure 6-8. 64-Bit IDT Gate Descriptors
 #[bitfield(u128)]
 pub struct Descriptor {
-    offset_in_segment0: u16,
+    offset0: u16,
     segment_selector: u16,
     #[bits(3)]
     ist: u8,
@@ -22,18 +25,41 @@ pub struct Descriptor {
     dpl: u8,
     p: bool,
     #[bits(48)]
-    offset_in_segment1: u64,
+    offset1: u64,
     reserved2: u32,
 }
 
 #[derive(Debug)]
 pub struct Debug {
+    #[allow(dead_code)]
+    offset: u64,
+    #[allow(dead_code)]
+    segment_selector: u16,
+    #[allow(dead_code)]
+    interrupt_stack_table: u8,
+    #[allow(dead_code)]
+    descriptor_type: Type,
+    #[allow(dead_code)]
+    dpl: u8,
 }
 
 impl From<&Descriptor> for Option<Debug> {
     fn from(descriptor: &Descriptor) -> Self {
         if descriptor.p() {
+            let offset0: u64 = descriptor.offset0() as u64;
+            let offset1: u64 = descriptor.offset1();
+            let offset: u64 = offset0 + (offset1 << Descriptor::OFFSET0_OFFSET);
+            let segment_selector: u16 = descriptor.segment_selector();
+            let interrupt_stack_table: u8 = descriptor.ist();
+            let descriptor_type: u8 = descriptor.descriptor_type();
+            let descriptor_type = Type::new(descriptor_type, false, false, false);
+            let dpl: u8 = descriptor.dpl();
             Some(Debug {
+                offset,
+                segment_selector,
+                interrupt_stack_table,
+                descriptor_type,
+                dpl,
             })
         } else {
             None
