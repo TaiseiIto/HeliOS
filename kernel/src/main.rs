522564dfd88bf170530f1239109ac412f296423f
TaiseiIto <taisei1212@outlook.jp>
2024-01-29 20:56:36 +0900

    Create a kernel #11

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,3 +1,25 @@
+//! The kernel
+
+#![no_main]
+#![no_std]
+
+use core::{
+    arch::asm,
+    panic::PanicInfo,
+};
+
+#[no_mangle]
 fn main() {
-    println!("Hello, world!");
+    panic!("Kernel Panic!");
 }
+
+/// # A panic handler of the kernel
+#[panic_handler]
+fn panic(panic: &PanicInfo) -> ! {
+    loop {
+        unsafe {
+            asm!("hlt");
+        }
+    }
+}
+
