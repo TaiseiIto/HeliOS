9bbceaab5ed275f217c8a5cbd9a6bbb05b6ad9b6
TaiseiIto <taisei1212@outlook.jp>
2024-01-30 19:35:04 +0900

    Read a file protocol #11

--- boot/src/efi/protocol/file.rs
+++ boot/src/efi/protocol/file.rs
@@ -5,6 +5,8 @@
 use {
     bitfield_struct::bitfield,
     super::super::{
+        Event,
+        Guid,
         Status,
         Void,
         char16,
@@ -23,6 +25,15 @@ pub struct Protocol {
     delete: Delete,
     read: Read,
     write: Write,
+    get_position: GetPosition,
+    set_position: SetPosition,
+    get_info: GetInfo,
+    set_info: SetInfo,
+    flush: Flush,
+    open_ex: OpenEx,
+    read_ex: ReadEx,
+    write_ex: WriteEx,
+    flush_ex: FlushEx,
 }
 
 /// # EFI_FILE_OPEN
@@ -76,4 +87,61 @@ type Read = extern "efiapi" fn(/* This */ &Protocol, /* BufferSize */ &mut usize
 /// ## References
 /// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 13.5 File Protocol
 type Write = extern "efiapi" fn(/* This */ &Protocol, /* BufferSize */ &mut usize, /* Buffer */ &Void) -> Status;
+
+/// # EFI_FILE_OPEN_EX
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 13.5 File Protocol
+type OpenEx = extern "efiapi" fn(/* This */ &Protocol, /* NewHandle */ &mut &Protocol, /* FileName */ char16::NullTerminatedString, /* OpenMode */OpenMode, /* Attributes */ Attributes, /* Token */ &mut IoToken) -> Status;
+
+/// # EFI_FILE_IO_TOKEN
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 13.5 File Protocol
+#[derive(Debug)]
+#[repr(C)]
+pub struct IoToken<'a> {
+    event: Event<'a>,
+    status: Status,
+    buffer_size: usize,
+    buffer: &'a Void,
+}
+
+/// # EFI_FILE_READ_EX
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 13.5 File Protocol
+type ReadEx = extern "efiapi" fn(/* This */ &Protocol, /* Token */ &mut IoToken) -> Status;
+
+/// # EFI_FILE_WRITE_EX
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 13.5 File Protocol
+type WriteEx = extern "efiapi" fn(/* This */ &Protocol, /* Token */ &mut IoToken) -> Status;
+
+/// # EFI_FILE_FLUSH_EX
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 13.5 File Protocol
+type FlushEx = extern "efiapi" fn(/* This */ &Protocol, /* Token */ &mut IoToken) -> Status;
+
+/// # EFI_FILE_SET_POSITION
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 13.5 File Protocol
+type SetPosition = extern "efiapi" fn(/* This */ &Protocol, /* Position */ u64) -> Status;
+
+/// # EFI_FILE_GET_POSITION
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 13.5 File Protocol
+type GetPosition = extern "efiapi" fn(/* This */ &Protocol, /* Position */ &mut u64) -> Status;
+
+/// # EFI_FILE_GET_INFO
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 13.5 File Protocol
+type GetInfo = extern "efiapi" fn(/* This */ &Protocol, /* InformationType */ &Guid, /* BufferSize */ &mut usize, /* Buffer */ &mut Void) -> Status;
+
+/// # EFI_FILE_SET_INFO
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 13.5 File Protocol
+type SetInfo = extern "efiapi" fn(/* This */ &Protocol, /* InformationType */ &Guid, /* BufferSize */ usize, /* Buffer */ &Void) -> Status;
+
+/// # EFI_FILE_FLUSH
+/// ## References
+/// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 13.5 File Protocol
+type Flush = extern "efiapi" fn(/* This */ &Protocol) -> Status;
 
--- boot/src/efi/void.rs
+++ boot/src/efi/void.rs
@@ -13,6 +13,7 @@ pub fn null<T>() -> &'static T {
 /// # CHAR16
 /// ## References
 /// * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf) 2.3.1 Data Types
+#[derive(Debug)]
 pub struct Void;
 
 impl Void {
