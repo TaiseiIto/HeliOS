1a5ba787549a48febb7e04c946a0df74c38969ff
TaiseiIto <taisei1212@outlook.jp>
2024-02-01 20:37:31 +0900

    Read kernel.elf #11

--- boot/src/elf.rs
+++ boot/src/elf.rs
@@ -14,10 +14,22 @@ pub struct File {
     bytes: Vec<u8>,
 }
 
+impl File {
+    fn header(&self) -> &Header {
+        let header: &u8 = &self.bytes[0];
+        let header: *const u8 = header as *const u8;
+        let header: *const Header = header as *const Header;
+        unsafe {
+            &*header
+        }
+    }
+}
+
 impl fmt::Debug for File {
     fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
         formatter
             .debug_struct("File")
+            .field("header", self.header())
             .finish()
     }
 }
@@ -28,5 +40,33 @@ impl From<Vec<u8>> for File {
             bytes,
         }
     }
+}
+
+type Addr = u32;
+type Half = u16;
+type Off = u32;
+type Sword = i32;
+type Word = u32;
+type UnsignedChar = u8;
+
+const EI_NIDENT: usize = 16;
+
+#[derive(Debug)]
+#[repr(C)]
+struct Header {
+    e_ident: [UnsignedChar; EI_NIDENT],
+    e_type: Half,
+    e_machine: Half,
+    e_version: Word,
+    e_entry: Addr,
+    e_phoff: Off,
+    e_shoff: Off,
+    e_flags: Word,
+    e_ehsize: Half,
+    e_phentsize: Half,
+    e_phnum: Half,
+    e_shentsize: Half,
+    e_shnum: Half,
+    e_shstrndx: Half,
 }
 
