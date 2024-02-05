d0eb02de166c886ff68ec441cacd01c7175a6acb
TaiseiIto <taisei1212@outlook.jp>
2024-02-05 22:06:31 +0900

    Debug #11

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -16,6 +16,103 @@ fn main() {
 /// # A panic handler of the kernel
 #[panic_handler]
 fn panic(_panic: &PanicInfo) -> ! {
+    unsafe {
+        asm!(
+            "out dx, al",
+            in("al") 0x48i8,
+            in("dx") 0x02f8i16,
+        );
+        asm!(
+            "out dx, al",
+            in("al") 0x65i8,
+            in("dx") 0x02f8i16,
+        );
+        asm!(
+            "out dx, al",
+            in("al") 0x6ci8,
+            in("dx") 0x02f8i16,
+        );
+        asm!(
+            "out dx, al",
+            in("al") 0x6ci8,
+            in("dx") 0x02f8i16,
+        );
+        asm!(
+            "out dx, al",
+            in("al") 0x6fi8,
+            in("dx") 0x02f8i16,
+        );
+        asm!(
+            "out dx, al",
+            in("al") 0x2ci8,
+            in("dx") 0x02f8i16,
+        );
+        asm!(
+            "out dx, al",
+            in("al") 0x20i8,
+            in("dx") 0x02f8i16,
+        );
+        asm!(
+            "out dx, al",
+            in("al") 0x6bi8,
+            in("dx") 0x02f8i16,
+        );
+        asm!(
+            "out dx, al",
+            in("al") 0x65i8,
+            in("dx") 0x02f8i16,
+        );
+        asm!(
+            "out dx, al",
+            in("al") 0x72i8,
+            in("dx") 0x02f8i16,
+        );
+        asm!(
+            "out dx, al",
+            in("al") 0x6ei8,
+            in("dx") 0x02f8i16,
+        );
+        asm!(
+            "out dx, al",
+            in("al") 0x65i8,
+            in("dx") 0x02f8i16,
+        );
+        asm!(
+            "out dx, al",
+            in("al") 0x6ci8,
+            in("dx") 0x02f8i16,
+        );
+        asm!(
+            "out dx, al",
+            in("al") 0x2ei8,
+            in("dx") 0x02f8i16,
+        );
+        asm!(
+            "out dx, al",
+            in("al") 0x65i8,
+            in("dx") 0x02f8i16,
+        );
+        asm!(
+            "out dx, al",
+            in("al") 0x6ci8,
+            in("dx") 0x02f8i16,
+        );
+        asm!(
+            "out dx, al",
+            in("al") 0x66i8,
+            in("dx") 0x02f8i16,
+        );
+        asm!(
+            "out dx, al",
+            in("al") 0x21i8,
+            in("dx") 0x02f8i16,
+        );
+        asm!(
+            "out dx, al",
+            in("al") 0x0ai8,
+            in("dx") 0x02f8i16,
+        );
+    }
     loop {
         unsafe {
             asm!("hlt");
