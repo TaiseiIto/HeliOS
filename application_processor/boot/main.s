c7473c5b888a99813dff13a00c68d6683f00b61e
TaiseiIto <taisei1212@outlook.jp>
2024-03-16 14:12:40 +0900

    Application processor boot loader #15

--- application_processor/boot/main.s
+++ application_processor/boot/main.s
@@ -1,9 +1,22 @@
 	.text
 	.code16
 main:
-0:
-	movw	0x7000,	%ss
-1:
+0:	# Initialize the general registers.
+	xorw	%ax,	%ax
+	movw	%ax,	%bx
+	movw	%ax,	%cx
+	movw	%ax,	%dx
+	movw	%ax,	%si
+	movw	%ax,	%di
+	movw	%ax,	%sp
+	movw	%ax,	%bp
+1:	# Initialize the segment registers.
+	movw	%ax,	%ds
+	movw	%ax,	%es
+	movw	%ax,	%fs
+	movw	%ax,	%gs
+	movw	%ax,	%ss
+2:	# Halt loop
 	hlt
-	jmp	1b
+	jmp	2b
 
