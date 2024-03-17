792ca0a4c78eeca85f59fd2d2557ba07fcfa1603
TaiseiIto <taisei1212@outlook.jp>
2024-03-17 16:10:11 +0900

    Application processor boot loader #15

--- boot/build.rs
+++ boot/build.rs
@@ -6,10 +6,14 @@ use std::{
 };
 
 fn main() {
+    let application_processor_boot_loader: String = env::var("APPLICATION_PROCESSOR_BOOT_LOADER").unwrap();
+    println!("application_processor_boot_loader = {:#x?}", application_processor_boot_loader);
     let application_processor_boot_loader_base: String = env::var("APPLICATION_PROCESSOR_BOOT_LOADER_BASE").unwrap();
     println!("application_processor_boot_loader_base = {:#x?}", application_processor_boot_loader_base);
     let application_processor_boot_loader_stack_floor: String = env::var("APPLICATION_PROCESSOR_BOOT_LOADER_STACK_FLOOR").unwrap();
     println!("application_processor_boot_loader_stack_floor = {:#x?}", application_processor_boot_loader_stack_floor);
+    let kernel: String = env::var("KERNEL").unwrap();
+    println!("kernel = {:#x?}", kernel);
     let out_dir: String = env::var("OUT_DIR").unwrap();
     println!("out_dir = {:#x?}", out_dir);
     let constants = Path::new(&out_dir).join("constants.rs");
