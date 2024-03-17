574e082df43561f7b1558280f909e2ecc595e2a2
TaiseiIto <taisei1212@outlook.jp>
2024-03-17 14:28:56 +0900

    Application processor boot loader #15

--- boot/build.rs
+++ boot/build.rs
@@ -1,11 +1,21 @@
-use std::env;
+use std::{
+    env,
+    fs::File,
+    io::Write,
+    path::Path,
+};
 
 fn main() {
     let application_processor_boot_loader_base: String = env::var("APPLICATION_PROCESSOR_BOOT_LOADER_BASE").unwrap();
-    let application_processor_boot_loader_stack_floor: String = env::var("APPLICATION_PROCESSOR_BOOT_LOADER_STACK_FLOOR").unwrap();
-    let out_dir: String = env::var("OUT_DIR").unwrap();
     println!("application_processor_boot_loader_base = {:#x?}", application_processor_boot_loader_base);
+    let application_processor_boot_loader_stack_floor: String = env::var("APPLICATION_PROCESSOR_BOOT_LOADER_STACK_FLOOR").unwrap();
     println!("application_processor_boot_loader_stack_floor = {:#x?}", application_processor_boot_loader_stack_floor);
+    let out_dir: String = env::var("OUT_DIR").unwrap();
     println!("out_dir = {:#x?}", out_dir);
+    let constants = Path::new(&out_dir).join("constants.rs");
+    println!("constants = {:#x?}", constants);
+    let mut constants = File::create(&constants).unwrap();
+    writeln!(&mut constants, "const APPLICATION_PROCESSOR_BOOT_LOADER_BASE: usize = {};", application_processor_boot_loader_base).unwrap();
+    writeln!(&mut constants, "const APPLICATION_PROCESSOR_BOOT_LOADER_STACK_FLOOR: usize = {};", application_processor_boot_loader_stack_floor).unwrap();
 }
 
