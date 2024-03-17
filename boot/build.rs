acd87c9a31b8b1c433a0547657987fac54e39f6d
TaiseiIto <taisei1212@outlook.jp>
2024-03-17 14:06:30 +0900

    Application processor boot loader #15

--- boot/build.rs
+++ boot/build.rs
@@ -1,7 +1,11 @@
+use std::env;
+
 fn main() {
-    let application_processor_boot_loader_base: &str = option_env!("APPLICATION_PROCESSOR_BOOT_LOADER_BASE").unwrap();
-    let application_processor_boot_loader_stack_floor: &str = option_env!("APPLICATION_PROCESSOR_BOOT_LOADER_STACK_FLOOR").unwrap();
+    let application_processor_boot_loader_base: String = env::var("APPLICATION_PROCESSOR_BOOT_LOADER_BASE").unwrap();
+    let application_processor_boot_loader_stack_floor: String = env::var("APPLICATION_PROCESSOR_BOOT_LOADER_STACK_FLOOR").unwrap();
+    let out_dir: String = env::var("OUT_DIR").unwrap();
     println!("application_processor_boot_loader_base = {:#x?}", application_processor_boot_loader_base);
     println!("application_processor_boot_loader_stack_floor = {:#x?}", application_processor_boot_loader_stack_floor);
+    println!("out_dir = {:#x?}", out_dir);
 }
 
