0d57d5bd40dd60c0d3b170a91d23dfe7db098519
TaiseiIto <taisei1212@outlook.jp>
2024-03-19 21:27:05 +0900

    Get RSDT #15

--- kernel/src/acpi/root_system_description.rs
+++ kernel/src/acpi/root_system_description.rs
@@ -84,7 +84,6 @@ impl fmt::Debug for Pointer {
 /// # RSDT
 /// ## References
 /// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 5.2.7 Root System Description Table (RSDT)
-#[derive(Debug)]
 #[repr(packed)]
 pub struct Table {
     signature: [u8; 4],
@@ -92,9 +91,47 @@ pub struct Table {
     revision: u8,
     checksum: u8,
     oemid: [u8; 6],
-    oem_table_id: u64,
+    oem_table_id: [u8; 8],
     oem_revision: u32,
-    creater_id: u32,
+    creater_id: [u8; 4],
     creater_revision: u32,
+}
+
+impl Table {
+    fn creater_id(&self) -> &str {
+        str::from_utf8(self.creater_id.as_slice()).unwrap()
+    }
+
+    fn oemid(&self) -> &str {
+        str::from_utf8(self.oemid.as_slice()).unwrap()
+    }
+
+    fn oem_table_id(&self) -> &str {
+        str::from_utf8(self.oem_table_id.as_slice()).unwrap()
+    }
+
+    fn signature(&self) -> &str {
+        str::from_utf8(self.signature.as_slice()).unwrap()
+    }
+}
+
+impl fmt::Debug for Table {
+    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let length: u32 = self.length;
+        let oem_revision: u32 = self.oem_revision;
+        let creater_revision: u32 = self.creater_revision;
+        formatter
+            .debug_struct("RSDT")
+            .field("signature", &self.signature())
+            .field("length", &length)
+            .field("revision", &self.revision)
+            .field("checksum", &self.checksum)
+            .field("oemid", &self.oemid())
+            .field("oem_table_id", &self.oem_table_id())
+            .field("oem_revision", &oem_revision)
+            .field("creater_id", &self.creater_id())
+            .field("creater_revision", &creater_revision)
+            .finish()
+    }
 }
 
