dabf1c296ebb24a5787c2e1d0ad89c1d23f51558
TaiseiIto <taisei1212@outlook.jp>
2024-03-20 19:41:08 +0900

    Get MADT #15

--- kernel/src/acpi/multiple_apic_description.rs
+++ kernel/src/acpi/multiple_apic_description.rs
@@ -1,12 +1,16 @@
 use {
     bitfield_struct::bitfield,
+    core::{
+        fmt,
+        mem,
+        slice,
+    },
     super::system_description,
 };
 
 /// # MADT
 /// ## References
 /// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 5.2.12 Multiple APIC Description Table (MADT)
-#[derive(Debug)]
 #[repr(packed)]
 pub struct Table {
     header: system_description::Header,
@@ -15,8 +19,33 @@ pub struct Table {
 }
 
 impl Table {
+    pub fn bytes(&self) -> &[u8] {
+        let table: *const Self = self as *const Self;
+        let table: usize = table as usize;
+        let first_byte: usize = table + mem::size_of::<Self>();
+        let first_byte: *const u8 = first_byte as *const u8;
+        let size: usize = self.header.table_size() - mem::size_of::<Self>();
+        unsafe {
+            slice::from_raw_parts(first_byte, size)
+        }
+    }
+
     pub fn is_correct(&self) -> bool {
         self.header.is_correct()
+    }
+}
+
+impl fmt::Debug for Table {
+    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let local_interrupt_controller_address: u32 = self.local_interrupt_controller_address;
+        let flags: Flags = self.flags;
+        formatter
+            .debug_struct("Table")
+            .field("header", &self.header)
+            .field("local_interrupt_controller_address", &local_interrupt_controller_address)
+            .field("flags", &flags)
+            .field("bytes", &self.bytes())
+            .finish()
     }
 }
 
