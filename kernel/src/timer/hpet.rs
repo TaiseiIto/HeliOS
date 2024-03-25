a33af3a012481a04299e40e0b3ec5fcfe81e0b19
TaiseiIto <taisei1212@outlook.jp>
2024-03-25 21:36:41 +0900

    Get HPET registers #15

--- kernel/src/timer/hpet.rs
+++ kernel/src/timer/hpet.rs
@@ -1,12 +1,14 @@
 pub mod timer;
+pub mod general_capabilities_and_id;
+
+use core::fmt;
 
 /// # Register Overview
 /// ## References
 /// * [IA-PC HPET (High Precision Event Timers Specification)](https://www.intel.com/content/dam/www/public/us/en/documents/technical-specifications/software-developers-hpet-spec-1-0a.pdf) 2.3.1 Register Overview Table 2 Memory-Mapped Registers
-#[derive(Debug)]
 #[repr(packed)]
 pub struct Registers {
-    general_capabilities_and_id: u64,
+    general_capabilities_and_id: general_capabilities_and_id::Register,
     reserved0: u64,
     general_configuration: u64,
     reserved1: u64,
@@ -14,6 +16,24 @@ pub struct Registers {
     reserved2: [u64; 0x19],
     main_counter_value: u64,
     reserved3: u64,
-    timer: [timer::Registers; 0x18]
+    timer: [timer::Registers; 0x18],
+}
+
+impl fmt::Debug for Registers {
+    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let general_capabilities_and_id: general_capabilities_and_id::Register = self.general_capabilities_and_id;
+        let general_configuration: u64 = self.general_configuration;
+        let general_interrupt_status: u64 = self.general_interrupt_status;
+        let main_counter_value: u64 = self.main_counter_value;
+        let timer: [timer::Registers; 0x18] = self.timer;
+        formatter
+            .debug_struct("Registers")
+            .field("general_capabilities_and_id", &general_capabilities_and_id)
+            .field("general_configuration", &general_configuration)
+            .field("general_interrupt_status", &general_interrupt_status)
+            .field("main_counter_value", &main_counter_value)
+            .field("timer", &timer)
+            .finish()
+    }
 }
 
---
+++ kernel/src/timer/hpet/general_capabilities_and_id.rs
@@ -0,0 +1,18 @@
+use bitfield_struct::bitfield;
+
+/// # General Capabilities and ID Register
+/// ## References
+/// * [IA-PC HPET (High Precision Event Timers Specification)](https://www.intel.com/content/dam/www/public/us/en/documents/technical-specifications/software-developers-hpet-spec-1-0a.pdf) 2.3.4 General Capabilities and ID Register
+#[bitfield(u64)]
+pub struct Register {
+    ref_id: u8,
+    #[bits(5)]
+    num_tim_cap: u8,
+    count_size_cap: bool,
+    #[bits(access = RO)]
+    reserved: bool,
+    leg_route_cap: bool,
+    vendor_id: u16,
+    counter_clk_period: u32,
+}
+
