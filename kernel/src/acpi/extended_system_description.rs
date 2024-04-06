75eadd84efdcb640312050f7829fd7934a088a50
TaiseiIto <taisei1212@outlook.jp>
2024-04-06 14:42:03 +0900

    Get mut MADT #15

--- kernel/src/acpi/extended_system_description.rs
+++ kernel/src/acpi/extended_system_description.rs
@@ -5,7 +5,10 @@ use {
         mem,
         slice,
     },
-    super::system_description,
+    super::{
+        multiple_apic_description,
+        system_description,
+    },
 };
 
 /// # XSDT
@@ -41,6 +44,28 @@ impl Table {
             .all(|entry| entry.is_correct())
     }
 
+    pub fn madt_mut(&mut self) -> &mut multiple_apic_description::Table {
+        self.bytes_mut()
+            .chunks(mem::size_of::<usize>())
+            .find_map(|entry_address_bytes| {
+                let entry: usize = entry_address_bytes
+                    .iter()
+                    .rev()
+                    .fold(0usize, |entry_address, byte| (entry_address << u8::BITS) + (*byte as usize));
+                let header: *const system_description::Header = entry as *const system_description::Header;
+                let header: &system_description::Header = unsafe {
+                    &*header
+                };
+                (header.signature() == "APIC").then(|| {
+                    let table: *mut multiple_apic_description::Table = entry as *mut multiple_apic_description::Table;
+                    unsafe {
+                        &mut *table
+                    }
+                })
+            })
+            .unwrap()
+    }
+
     fn bytes(&self) -> &[u8] {
         let table: *const Self = self as *const Self;
         let table: usize = table as usize;
@@ -49,6 +74,17 @@ impl Table {
         let length: usize = self.header.table_size() - mem::size_of::<Self>();
         unsafe {
             slice::from_raw_parts(first_byte, length)
+        }
+    }
+
+    fn bytes_mut(&mut self) -> &mut [u8] {
+        let table: *mut Self = self as *mut Self;
+        let table: usize = table as usize;
+        let first_byte: usize = table + mem::size_of::<Self>();
+        let first_byte: *mut u8 = first_byte as *mut u8;
+        let length: usize = self.header.table_size() - mem::size_of::<Self>();
+        unsafe {
+            slice::from_raw_parts_mut(first_byte, length)
         }
     }
 }
