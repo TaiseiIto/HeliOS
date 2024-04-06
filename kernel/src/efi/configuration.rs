6dce6f2c223b714fde296f2ce7e5d1c372a5f605
TaiseiIto <taisei1212@outlook.jp>
2024-04-06 13:48:57 +0900

    Get mut rsdp #15

--- kernel/src/efi/configuration.rs
+++ kernel/src/efi/configuration.rs
@@ -7,22 +7,21 @@ use {
     },
 };
 
-#[derive(Clone)]
 #[repr(C)]
 pub struct Tables<'a> {
     number_of_table_entries: usize,
-    configuration_table: &'a Table,
+    configuration_table: &'a mut Table,
 }
 
 impl Tables<'_> {
-    pub fn rsdp(&self) -> &acpi::root_system_description::Pointer {
+    pub fn rsdp(&mut self) -> &mut acpi::root_system_description::Pointer {
         let acpi_table_guid = Guid::new(0x8868e871, 0xe4f1, 0x11d3, [0xbc, 0x22, 0x0, 0x80, 0xc7, 0x3c, 0x88, 0x81]);
-        let rsdp: *const acpi::root_system_description::Pointer = self.iter()
+        let rsdp: *mut acpi::root_system_description::Pointer = self.iter_mut()
             .find(|table| table.vendor_guid == acpi_table_guid)
             .unwrap()
-            .vendor_table as *const acpi::root_system_description::Pointer;
+            .vendor_table as *mut acpi::root_system_description::Pointer;
         unsafe {
-            &*rsdp
+            &mut *rsdp
         }
     }
 
@@ -33,6 +32,17 @@ impl Tables<'_> {
                 let table: *const Table = table as *const Table;
                 unsafe {
                     &*table.add(index)
+                }
+            })
+    }
+
+    pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut Table> {
+        (0..self.number_of_table_entries)
+            .map(|index| {
+                let table: &mut Table = self.configuration_table;
+                let table: *mut Table = table as *mut Table;
+                unsafe {
+                    &mut *table.add(index)
                 }
             })
     }
