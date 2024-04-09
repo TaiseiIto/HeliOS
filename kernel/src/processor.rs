30bb0df260feb3a43fc5bcf85d086ef2624c64a1
TaiseiIto <taisei1212@outlook.jp>
2024-04-09 12:55:54 +0900

    Debug #15

--- kernel/src/processor.rs
+++ kernel/src/processor.rs
@@ -18,12 +18,19 @@ impl Controller {
         let local_apic_id: u8 = self.local_apic_id() as u8;
         com2_println!("Boot processor {:#x?}", local_apic_id);
         let entry_point: usize = boot_loader.entry_point();
+        com2_println!("entry_point = {:#x?}", entry_point);
         local_apic_registers.send_init(local_apic_id);
+        com2_println!("Debug 0");
         hpet.wait_milliseconds(10);
+        com2_println!("Debug 1");
         local_apic_registers.send_sipi(local_apic_id, entry_point);
+        com2_println!("Debug 2");
         hpet.wait_microseconds(200);
+        com2_println!("Debug 3");
         local_apic_registers.send_sipi(local_apic_id, entry_point);
+        com2_println!("Debug 4");
         hpet.wait_microseconds(200);
+        com2_println!("Debug 5");
     }
 
     pub fn local_apic_id(&self) -> u8 {
