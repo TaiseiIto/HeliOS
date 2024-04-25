8a987f083b989c770a374a117fa05f54aca4b045
TaiseiIto <taisei1212@outlook.jp>
2024-04-25 20:16:16 +0900

    Application processor kernel #15

--- processor/kernel/src/main.rs
+++ processor/kernel/src/main.rs
@@ -1,3 +1,21 @@
+//! # The application processor kernel
+
+#![feature(abi_x86_interrupt)]
+#![no_main]
+#![no_std]
+
+mod x64;
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
 fn main() {
-    println!("Hello, world!");
+    panic!("End of kernel.elf");
+}
+
+#[panic_handler]
+fn panic(panic: &PanicInfo) -> ! {
+    loop {
+        x64::hlt();
+    }
 }
---
+++ processor/kernel/src/x64.rs
@@ -0,0 +1,16 @@
+//! # Wrapper functions of x64 instructions
+//! ## References
+//! * [Intel 64 and IA-32 Architectures Software Developer's Manual December 2023](https://www.intel.com/content/www/us/en/developer/articles/technical/intel-sdm.html)
+
+use core::arch::asm;
+
+/// # Halt
+/// ## References
+/// * [Intel 64 and IA-32 Architectures Software Developer's Manual December 2023](https://www.intel.com/content/www/us/en/developer/articles/technical/intel-sdm.html) Vol.2A 3-489
+#[inline(never)]
+pub fn hlt() {
+    unsafe {
+        asm!("hlt");
+    }
+}
+
