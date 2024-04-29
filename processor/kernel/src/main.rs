3342190dc42f97fdd3a9a865afac323aba3a27b4
TaiseiIto <taisei1212@outlook.jp>
2024-04-29 22:37:05 +0900

    Debug on QEMU #15

--- processor/kernel/src/main.rs
+++ processor/kernel/src/main.rs
@@ -22,11 +22,12 @@ pub struct Argument {
 #[no_mangle]
 fn main(argument: &'static mut Argument) {
     let mut ia32_apic_base: x64::msr::ia32::ApicBase = argument.ia32_apic_base;
-    let local_apic_registers: &interrupt::apic::local::Registers = ia32_apic_base.registers();
+    let local_apic_registers: &mut interrupt::apic::local::Registers = ia32_apic_base.registers_mut();
+    let local_apic_id: u8 = local_apic_registers.apic_id();
     unsafe {
         asm!(
-            "int 0x80",
-            in("rax") local_apic_registers,
+            "syscall",
+            in("al") local_apic_id,
         );
     }
     panic!("End of kernel.elf");
