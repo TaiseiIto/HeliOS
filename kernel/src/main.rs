30528db99ff9e2c2bd23e8f6834125dfcaf61b7f
TaiseiIto <taisei1212@outlook.jp>
2024-05-02 20:20:18 +0900

    Solve a bug on VMware #15

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -41,6 +41,7 @@ const PRIVILEGE_LEVEL: u8 = 0;
 #[no_mangle]
 fn main(argument: &'static mut Argument<'static>) {
     argument.set();
+    x64::cli();
     rs232c::set_com2(Argument::get().com2_mut());
     com2_println!("Hello from /HeliOS/kernel.elf");
     // Initialize allocator.
