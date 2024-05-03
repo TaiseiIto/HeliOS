31676860de4832a1ff7b2b4cbad715638f5dae2f
TaiseiIto <taisei1212@outlook.jp>
2024-05-03 14:22:42 +0900

    Debug on GPD MicroPC #15

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -190,7 +190,9 @@ fn main(argument: &'static mut Argument<'static>) {
     processor::Controller::set_all(processors);
     processor::Controller::get_all()
         .into_iter()
-        .for_each(|processor| processor.boot(Argument::get().processor_boot_loader_mut(), local_apic_registers, hpet, my_local_apic_id));
+        .next()
+        .unwrap()
+        .boot(Argument::get().processor_boot_loader_mut(), local_apic_registers, hpet, my_local_apic_id);
     let local_apic_id2log: BTreeMap<u8, &str> = processor::Controller::get_all()
         .into_iter()
         .map(|processor| (processor.local_apic_id(), processor.log()))
