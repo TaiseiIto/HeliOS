ae256395689e5ec4dc54e4c46a9d940836612414
TaiseiIto <taisei1212@outlook.jp>
2024-05-28 20:20:26 +0900

    Refactoring

--- boot/src/efi.rs
+++ boot/src/efi.rs
@@ -2,7 +2,6 @@
 //! ## References
 //! * [UEFI Specification Version 2.9](https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf)
 
-mod allocator;
 mod boot_services;
 mod char16;
 mod char8;
