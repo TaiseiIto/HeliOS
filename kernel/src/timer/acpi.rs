458d43543a9ee34ff21b2149f39e21aa1a09db81
TaiseiIto <taisei1212@outlook.jp>
2024-06-30 04:57:30 +0000

    ACPI timer #15

--- kernel/src/timer/acpi.rs
+++ kernel/src/timer/acpi.rs
@@ -7,7 +7,10 @@ use crate::{
     acpi,
     com2_print,
     com2_println,
+    x64,
 };
+
+const FREQUENCY: usize = 3579545; // Hz
 
 pub fn read_counter_value() -> u32 {
     Argument::get()
@@ -27,5 +30,32 @@ pub fn bits() -> usize {
         .xsdt()
         .fadt()
         .timer_bits()
+}
+
+pub fn wait_microseconds(microseconds: usize) {
+    let current_counter_value: usize = read_counter_value() as usize;
+    let bits: usize = bits();
+    let increments: usize = microseconds * FREQUENCY / 1000000;
+    assert!(increments < 1 << (bits - 1));
+    let minimum_counter_value: usize = (current_counter_value + increments) % (1 << bits);
+    let maximum_counter_value: usize = (minimum_counter_value + (1 << (bits - 1))) % (1 << bits);
+    while {
+        let current_counter_value: usize = read_counter_value() as usize;
+        if minimum_counter_value < maximum_counter_value {
+            !(minimum_counter_value..maximum_counter_value).contains(&current_counter_value)
+        } else {
+            (maximum_counter_value..minimum_counter_value).contains(&current_counter_value)
+        }
+    } {
+        x64::pause();
+    }
+}
+
+pub fn wait_milliseconds(milliseconds: usize) {
+    wait_microseconds(1000 * milliseconds)
+}
+
+pub fn wait_seconds(seconds: usize) {
+    wait_milliseconds(1000 * seconds)
 }
 
