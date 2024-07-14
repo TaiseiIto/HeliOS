8e0ca310df019cb2d680fe7ad93473c696b0c5fc
TaiseiIto <taisei1212@outlook.jp>
2024-07-14 07:36:46 +0000

    Analyze AML #15

--- kernel/src/acpi/machine_language/term_list.rs
+++ kernel/src/acpi/machine_language/term_list.rs
@@ -1,18 +1,25 @@
+use core::fmt;
+
 /// # ACPI Machine Lnaguage
 /// ## References
 /// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.5 Term Objects Encoding
-#[derive(Debug)]
 pub enum Symbol<'a> {
     Nothing,
-    TermObj {
+    TermObjTermList {
         bytes: &'a [u8],
     },
+}
+
+impl fmt::Debug for Symbol<'_> {
+    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(formatter, "TermList")
+    }
 }
 
 impl<'a> From<&'a [u8]> for Symbol<'a> {
     fn from(bytes: &'a [u8]) -> Self {
         match bytes.first() {
-            Some(first_byte) => Self::TermObj {
+            Some(first_byte) => Self::TermObjTermList {
                 bytes,
             },
             None => Self::Nothing,
