c8c7741b028dcbd0f9cdce85ef2afd5b6db1f1b3
TaiseiIto <taisei1212@outlook.jp>
2024-07-31 11:58:24 +0000

    Analyze AML #15

--- kernel/acpi_machine_language/src/lib.rs
+++ kernel/acpi_machine_language/src/lib.rs
@@ -24,11 +24,57 @@ pub fn acpi_machine_language(input: TokenStream) -> TokenStream {
             syn::Fields::Unnamed(syn::FieldsUnnamed {
                 paren_token,
                 unnamed,
-            }) => quote! {
-            }.into(),
+            }) => unnamed
+                .iter()
+                .for_each(|field| {
+                    let syn::Field {
+                        attrs,
+                        vis,
+                        ident,
+                        colon_token,
+                        ty,
+                        mutability,
+                    } = field;
+                    match ty {
+                        syn::Type::Path(syn::TypePath {
+                            qself,
+                            path,
+                        }) => {
+                            let syn::Path {
+                                leading_colon,
+                                segments,
+                            } = path;
+                            segments
+                                .iter()
+                                .for_each(|path_segment| {
+                                    let syn::PathSegment {
+                                        ident, // Vec
+                                        arguments,
+                                    } = path_segment;
+                                    match arguments {
+                                        syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
+                                            colon2_token,
+                                            lt_token,
+                                            args,
+                                            gt_token,
+                                        }) => args
+                                            .iter()
+                                            .for_each(|generic_argument| match generic_argument {
+                                                syn::GenericArgument::Type(child_type) => {}, // TermObj
+                                                _ => unimplemented!(),
+                                            }),
+                                        _ => unimplemented!(),
+                                    }
+                                });
+                        },
+                        _ => unimplemented!(),
+                    }
+                }),
             _ => unimplemented!(),
         },
         _ => unimplemented!(),
     }
+    quote! {
+    }.into()
 }
 
