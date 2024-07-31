0761ce032548dcce8c76b2d147fb32b6cadbc47b
TaiseiIto <taisei1212@outlook.jp>
2024-07-31 15:33:54 +0000

    Analyze AML #15

--- kernel/acpi_machine_language/src/lib.rs
+++ kernel/acpi_machine_language/src/lib.rs
@@ -17,7 +17,7 @@ use {
     },
 };
 
-#[proc_macro_derive(Symbol)]
+#[proc_macro_derive(Reader)]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let derive_input: DeriveInput = parse(input).unwrap();
     let debug: proc_macro2::TokenStream = derive_debug(&derive_input);
@@ -97,11 +97,41 @@ fn derive_debug(derive_input: &DeriveInput) -> proc_macro2::TokenStream {
 
 fn derive_from_slice_u8(derive_input: &DeriveInput) -> proc_macro2::TokenStream {
     quote! {
+        impl From<&[u8]> for TermList {
+            fn from(aml: &[u8]) -> Self {
+                assert!(Self::matches(aml), "aml = {:#x?}", aml);
+                let mut aml: &[u8] = aml;
+                let mut term_list: Vec<TermObj> = Vec::new();
+                while !aml.is_empty() {
+                    let (term_obj, remaining_aml): (TermObj, &[u8]) = TermObj::read(aml);
+                    com2_println!("term_obj = {:#x?}", term_obj);
+                    aml = remaining_aml;
+                    term_list.push(term_obj);
+                }
+                Self(term_list)
+            }
+        }
     }
 }
 
 fn derive_reader(derive_input: &DeriveInput) -> proc_macro2::TokenStream {
     quote! {
+        impl Reader<'_> for TermList {
+            fn length(&self) -> usize {
+                self.0
+                    .iter()
+                    .map(|term_obj| term_obj.length())
+                    .sum::<usize>()
+            }
+        
+            fn matches(aml: &[u8]) -> bool {
+                if aml.is_empty() {
+                    true
+                } else {
+                    TermObj::matches(aml)
+                }
+            }
+        }
     }
 }
 
--- kernel/src/acpi/machine_language/term_list.rs
+++ kernel/src/acpi/machine_language/term_list.rs
@@ -1,5 +1,5 @@
 use {
-    acpi_machine_language::Symbol,
+    acpi_machine_language::Reader,
     alloc::vec::Vec,
     crate::{
         com2_print,
@@ -14,38 +14,6 @@ use {
 /// # TermList
 /// ## References
 /// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.5 Term Objects Encoding
-#[derive(Symbol)]
+#[derive(Reader)]
 pub struct TermList(Vec<TermObj>);
-
-impl From<&[u8]> for TermList {
-    fn from(aml: &[u8]) -> Self {
-        assert!(Self::matches(aml), "aml = {:#x?}", aml);
-        let mut aml: &[u8] = aml;
-        let mut term_list: Vec<TermObj> = Vec::new();
-        while !aml.is_empty() {
-            let (term_obj, remaining_aml): (TermObj, &[u8]) = TermObj::read(aml);
-            com2_println!("term_obj = {:#x?}", term_obj);
-            aml = remaining_aml;
-            term_list.push(term_obj);
-        }
-        Self(term_list)
-    }
-}
-
-impl Reader<'_> for TermList {
-    fn length(&self) -> usize {
-        self.0
-            .iter()
-            .map(|term_obj| term_obj.length())
-            .sum::<usize>()
-    }
-
-    fn matches(aml: &[u8]) -> bool {
-        if aml.is_empty() {
-            true
-        } else {
-            TermObj::matches(aml)
-        }
-    }
-}
 
