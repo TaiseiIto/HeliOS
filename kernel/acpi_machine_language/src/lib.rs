4ab3013cc7221ab3741e818b2132747906a51079
TaiseiIto <taisei1212@outlook.jp>
2024-08-01 11:59:01 +0000

    Analyze AML #15

--- kernel/acpi_machine_language/src/lib.rs
+++ kernel/acpi_machine_language/src/lib.rs
@@ -7,12 +7,20 @@ use {
         quote,
     },
     syn::{
+        AngleBracketed,
+        AngleBracketedGenericArguments,
         Data,
         DataStruct,
         DeriveInput,
+        Field,
         Fields,
         FieldsUnnamed,
+        GenericArgument,
         Ident,
+        Path,
+        PathSegment,
+        Type,
+        TypePath,
         parse,
     },
 };
@@ -65,7 +73,7 @@ fn derive_debug(derive_input: &DeriveInput) -> proc_macro2::TokenStream {
                             .field(#field_name)
                         })
                         .collect();
-                    let format_fields = quote! {
+                    let format_fields: proc_macro2::TokenStream = quote! {
                         #(#format_fields)*
                     };
                     (unpack, format_fields)
@@ -105,19 +113,99 @@ fn derive_from_slice_u8(derive_input: &DeriveInput) -> proc_macro2::TokenStream 
         generics,
         data,
     } = derive_input;
+    let (convert, pack): (proc_macro2::TokenStream, proc_macro2::TokenStrem) = match data {
+        Data::Struct(DataStruct {
+            struct_token,
+            fields,
+            semi_token,
+        }) => match fields {
+            Fields::Unnamed(FieldsUnnamed {
+                paren_token,
+                unnamed,
+            }) => {
+                let (convert, pack): (Vec<proc_macro2::TokenStream>, Vec<proc_macro2::TokenStream>) = unnamed
+                    .iter()
+                    .enumerate()
+                    .map(|(index, field)| {
+                        let field_name: Ident = format_ident!("field{}", index);
+                        let Field {
+                            attrs,
+                            vis,
+                            ident,
+                            colon_token,
+                            ty,
+                        } = field;
+                        let convert: proc_macro2::TokenStream = match ty {
+                            Type::Path(TypePath {
+                                qself,
+                                path,
+                            }) => {
+                                let Path {
+                                    leading_colon,
+                                    segments,
+                                } = path;
+                                let PathSegment {
+                                    ident,
+                                    arguments,
+                                } = segments
+                                    .iter()
+                                    .last()
+                                    .unwrap();
+                                match ident.to_string().as_str() {
+                                    "Vec" => match argument {
+                                        PathArguments::AngleBracketed(AngleBracketedGenericArguments {
+                                            colon2_token,
+                                            lt_token,
+                                            args,
+                                            gt_token,
+                                        }) => match args.first().unwrap() {
+                                            GenericArgument::Type(element_type) => quote! {
+                                                let mut aml: &[u8] = aml;
+                                                let mut #field_name: Vec<#element_type> = Vec::new();
+                                                while !aml.is_empty() {
+                                                    let (element, remaining_aml): (#element_type, &[u8]) = #element_type::read(aml);
+                                                    crate::com2_println!("element = {:#x?}", element);
+                                                    aml = remaining_aml;
+                                                    #field_name.push(element);
+                                                }
+                                            },
+                                            _ => unimplemented!(),
+                                        },
+                                        _ => unimplemented!(),
+                                    },
+                                    _ => unimplemented!(),
+                                }
+                            },
+                            _ => unimplemented!(),
+                        };
+                        let pack: proc_macro2::TokenStream = quote! {
+                            #field_name
+                        };
+                        (convert, pack)
+                    })
+                    .fold((Vec::new(), Vec::new()), |(mut convert, mut pack), (new_convert, new_pack)| {
+                        convert.push(new_convert);
+                        pack.push(new_pack);
+                        (convert, pack)
+                    });
+                let convert: proc_macro2::TokenStream = quote! {
+                    #(#convert)*
+                };
+                let pack: proc_macro2::TokenStream = quote! {
+                    (#(#pack),*)
+                };
+                (convert, pack)
+            },
+            _ => unimplemented!(),
+        },
+        _ => unimplemented!(),
+    }
     quote! {
         impl From<&[u8]> for #ident {
             fn from(aml: &[u8]) -> Self {
                 assert!(Self::matches(aml), "aml = {:#x?}", aml);
-                let mut aml: &[u8] = aml;
-                let mut term_list: Vec<TermObj> = Vec::new();
-                while !aml.is_empty() {
-                    let (term_obj, remaining_aml): (TermObj, &[u8]) = TermObj::read(aml);
-                    crate::com2_println!("term_obj = {:#x?}", term_obj);
-                    aml = remaining_aml;
-                    term_list.push(term_obj);
-                }
-                Self(term_list)
+                #convert
+                Self #pack
             }
         }
     }
