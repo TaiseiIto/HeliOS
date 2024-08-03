447b0ea3aa0e3440d1bf88c8184eed380a926bbf
TaiseiIto <taisei1212@outlook.jp>
2024-08-03 13:25:29 +0000

    Analyze AML #15

--- kernel/src/acpi/machine_language/device_op_suffix.rs
+++ kernel/src/acpi/machine_language/device_op_suffix.rs
@@ -1,29 +1,7 @@
-use super::Reader;
-
-const DEVICE_OP_SUFFIX: u8 = 0x82;
-
 /// # DeviceOpSuffix
 /// ## References
 /// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.5.2 Named Objects Encoding
-#[derive(Debug)]
+#[derive(acpi_machine_language::Reader)]
+#[encoding_value = 0x82]
 pub struct DeviceOpSuffix;
-
-impl From<&[u8]> for DeviceOpSuffix {
-    fn from(aml: &[u8]) -> Self {
-        assert!(Self::matches(aml), "aml = {:#x?}", aml);
-        Self
-    }
-}
-
-impl Reader<'_> for DeviceOpSuffix {
-    fn length(&self) -> usize {
-        1
-    }
-
-    fn matches(aml: &[u8]) -> bool {
-        aml
-            .first()
-            .is_some_and(|head| *head == DEVICE_OP_SUFFIX)
-    }
-}
 
--- kernel/src/acpi/machine_language/field_op_suffix.rs
+++ kernel/src/acpi/machine_language/field_op_suffix.rs
@@ -1,29 +1,7 @@
-use super::Reader;
-
-const FIELD_OP_SUFFIX: u8 = 0x81;
-
 /// # FieldOpSuffix
 /// ## References
 /// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.5.2 Named Objects Encoding
-#[derive(Debug)]
+#[derive(acpi_machine_language::Reader)]
+#[encoding_value = 0x81]
 pub struct FieldOpSuffix;
-
-impl From<&[u8]> for FieldOpSuffix {
-    fn from(aml: &[u8]) -> Self {
-        assert!(Self::matches(aml), "aml = {:#x?}", aml);
-        Self
-    }
-}
-
-impl Reader<'_> for FieldOpSuffix {
-    fn length(&self) -> usize {
-        1
-    }
-
-    fn matches(aml: &[u8]) -> bool {
-        aml
-            .first()
-            .is_some_and(|head| *head == FIELD_OP_SUFFIX)
-    }
-}
 
