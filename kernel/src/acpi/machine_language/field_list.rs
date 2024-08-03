2aa588be0685c3a9cbf93b6b5d8859bb7697aad9
TaiseiIto <taisei1212@outlook.jp>
2024-08-03 06:29:23 +0000

    Analyze AML #15

--- kernel/src/acpi/machine_language/field_list.rs
+++ kernel/src/acpi/machine_language/field_list.rs
@@ -1,45 +1,11 @@
 use {
     alloc::vec::Vec,
-    super::{
-        FieldElement,
-        Reader,
-    },
+    super::FieldElement,
 };
 
 /// # FieldList
 /// ## References
 /// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.5.2 Named Objects Encoding
-#[derive(Debug)]
+#[derive(acpi_machine_language::Reader)]
 pub struct FieldList(Vec<FieldElement>);
-
-impl From<&[u8]> for FieldList {
-    fn from(aml: &[u8]) -> Self {
-        assert!(Self::matches(aml), "aml = {:#x?}", aml);
-        let mut aml: &[u8] = aml;
-        let mut field_list: Vec<FieldElement> = Vec::new();
-        while !aml.is_empty() {
-            let (field_element, remaining_aml): (FieldElement, &[u8]) = FieldElement::read(aml);
-            aml = remaining_aml;
-            field_list.push(field_element);
-        }
-        Self(field_list)
-    }
-}
-
-impl Reader<'_> for FieldList {
-    fn length(&self) -> usize {
-        self.0
-            .iter()
-            .map(|field_element| field_element.length())
-            .sum::<usize>()
-    }
-
-    fn matches(aml: &[u8]) -> bool {
-        if aml.first().is_some() {
-            FieldElement::matches(aml)
-        } else {
-            true
-        }
-    }
-}
 
--- kernel/src/acpi/machine_language/package_element_list.rs
+++ kernel/src/acpi/machine_language/package_element_list.rs
@@ -1,45 +1,11 @@
 use {
     alloc::vec::Vec,
-    super::{
-        PackageElement,
-        Reader,
-    },
+    super::PackageElement,
 };
 
 /// # PackageElementList
 /// ## References
 /// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.5.4 Expression Opcodes Encoding
-#[derive(Debug)]
+#[derive(acpi_machine_language::Reader)]
 pub struct PackageElementList(Vec<PackageElement>);
-
-impl From<&[u8]> for PackageElementList {
-    fn from(aml: &[u8]) -> Self {
-        assert!(Self::matches(aml), "aml = {:#x?}", aml);
-        let mut aml: &[u8] = aml;
-        let mut package_element_list: Vec<PackageElement> = Vec::new();
-        while !aml.is_empty() {
-            let (package_element, remaining_aml): (PackageElement, &[u8]) = PackageElement::read(aml);
-            aml = remaining_aml;
-            package_element_list.push(package_element);
-        }
-        Self(package_element_list)
-    }
-}
-
-impl Reader<'_> for PackageElementList {
-    fn length(&self) -> usize {
-        self.0
-            .iter()
-            .map(|package_element| package_element.length())
-            .sum::<usize>()
-    }
-
-    fn matches(aml: &[u8]) -> bool {
-        if aml.is_empty() {
-            true
-        } else {
-            PackageElement::matches(aml)
-        }
-    }
-}
 
