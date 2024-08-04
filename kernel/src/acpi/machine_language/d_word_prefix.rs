5856e38aeb6b380fc468ddee9fe919438b89eb3d
TaiseiIto <taisei1212@outlook.jp>
2024-08-04 06:53:13 +0000

    Analyze AML #15

--- kernel/src/acpi/machine_language/d_word_prefix.rs
+++ kernel/src/acpi/machine_language/d_word_prefix.rs
@@ -1,29 +1,7 @@
-use super::Reader;
-
-const D_WORD_PREFIX: u8 = 0x0c;
-
 /// # DWordPrefix
 /// ## References
 /// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.3 Data Objects Encoding
-#[derive(Debug)]
+#[derive(acpi_machine_language::Reader)]
+#[encoding_value = 0x0c]
 pub struct DWordPrefix;
-
-impl From<&[u8]> for DWordPrefix {
-    fn from(aml: &[u8]) -> Self {
-        assert!(Self::matches(aml), "aml = {:#x?}", aml);
-        Self
-    }
-}
-
-impl Reader<'_> for DWordPrefix {
-    fn length(&self) -> usize {
-        1
-    }
-
-    fn matches(aml: &[u8]) -> bool {
-        aml
-            .first()
-            .is_some_and(|head| *head == D_WORD_PREFIX)
-    }
-}
 
--- kernel/src/acpi/machine_language/null_char.rs
+++ kernel/src/acpi/machine_language/null_char.rs
@@ -1,29 +1,7 @@
-use super::Reader;
-
-const NULL_CHAR: u8 = 0x00;
-
 /// # NullChar
 /// ## References
 /// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.3 Data Objects Encoding
-#[derive(Debug)]
+#[derive(acpi_machine_language::Reader)]
+#[encoding_value = 0x00]
 pub struct NullChar;
-
-impl From<&[u8]> for NullChar {
-    fn from(aml: &[u8]) -> Self {
-        assert!(Self::matches(aml), "aml = {:#x?}", aml);
-        Self
-    }
-}
-
-impl Reader<'_> for NullChar {
-    fn length(&self) -> usize {
-        1
-    }
-
-    fn matches(aml: &[u8]) -> bool {
-        aml
-            .first()
-            .is_some_and(|head| *head == NULL_CHAR)
-    }
-}
 
