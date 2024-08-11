2254eaa344b07366653d4f46343b729759b57eaf
TaiseiIto <taisei1212@outlook.jp>
2024-08-11 12:24:57 +0000

    Analyze AML #15

--- kernel/src/acpi/machine_language/byte_data.rs
+++ kernel/src/acpi/machine_language/byte_data.rs
@@ -1,31 +1,14 @@
-use super::Reader;
-
 /// # ByteData
 /// ## References
 /// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.3 Data Objects Encoding
-#[derive(Debug)]
+#[derive(acpi_machine_language::Reader)]
+#[encoding_value_min = 0x00]
+#[encoding_value_max = 0xff]
 pub struct ByteData(u8);
 
 impl ByteData {
     pub fn pkg_length(&self) -> usize {
         self.0 as usize
-    }
-}
-
-impl From<&[u8]> for ByteData {
-    fn from(aml: &[u8]) -> Self {
-        assert!(Self::matches(aml), "aml = {:#x?}", aml);
-        Self(*aml.first().unwrap())
-    }
-}
-
-impl Reader<'_> for ByteData {
-    fn length(&self) -> usize {
-        1
-    }
-
-    fn matches(_aml: &[u8]) -> bool {
-        true
     }
 }
 
