c0dd89989a3deb0488a041da98f58277e1b4bd4f
TaiseiIto <taisei1212@outlook.jp>
2024-08-11 12:18:00 +0000

    Analyze AML #15

--- kernel/src/acpi/machine_language/def_index.rs
+++ kernel/src/acpi/machine_language/def_index.rs
@@ -1,11 +1,9 @@
 use {
     alloc::boxed::Box,
-    core::fmt,
     super::{
         BuffPkgStrObj,
         IndexOp,
         IndexValue,
-        Reader,
         Target,
     },
 };
@@ -13,63 +11,11 @@ use {
 /// # DefIndex
 /// ## References
 /// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.5.4 Expression Opcodes Encoding
-pub struct DefIndex {
-    index_op: IndexOp,
-    buff_pkg_str_obj: BuffPkgStrObj,
-    index_value: IndexValue,
-    target: Box<Target>,
-}
-
-impl fmt::Debug for DefIndex {
-    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let Self {
-            index_op,
-            buff_pkg_str_obj,
-            index_value,
-            target,
-        } = self;
-        formatter
-            .debug_tuple("DefIndex")
-            .field(index_op)
-            .field(buff_pkg_str_obj)
-            .field(index_value)
-            .field(target)
-            .finish()
-    }
-}
-
-impl From<&[u8]> for DefIndex {
-    fn from(aml: &[u8]) -> Self {
-        let (index_op, aml): (IndexOp, &[u8]) = IndexOp::read(aml);
-        let (buff_pkg_str_obj, aml): (BuffPkgStrObj, &[u8]) = BuffPkgStrObj::read(aml);
-        let (index_value, aml): (IndexValue, &[u8]) = IndexValue::read(aml);
-        let (target, _aml): (Target, &[u8]) = Target::read(aml);
-        let target: Box<Target> = Box::new(target);
-        Self {
-            index_op,
-            buff_pkg_str_obj,
-            index_value,
-            target,
-        }
-    }
-}
-
-impl Reader<'_> for DefIndex {
-    fn length(&self) -> usize {
-        let Self {
-            index_op,
-            buff_pkg_str_obj,
-            index_value,
-            target,
-        } = self;
-        index_op.length()
-        + buff_pkg_str_obj.length()
-        + index_value.length()
-        + target.length()
-    }
-
-    fn matches(aml: &[u8]) -> bool {
-        IndexOp::matches(aml)
-    }
-}
+#[derive(acpi_machine_language::Reader)]
+pub struct DefIndex(
+    IndexOp,
+    BuffPkgStrObj,
+    IndexValue,
+    Box<Target>,
+);
 
--- kernel/src/acpi/machine_language/index_value.rs
+++ kernel/src/acpi/machine_language/index_value.rs
@@ -1,31 +1,11 @@
 use {
     alloc::boxed::Box,
-    super::{
-        Reader,
-        TermArg,
-    },
+    super::TermArg,
 };
 
 /// # IndexValue
 /// ## References
 /// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.5.4 Expression Opcodes Encoding
-#[derive(Debug)]
+#[derive(acpi_machine_language::Reader)]
 pub struct IndexValue(Box<TermArg>);
-
-impl From<&[u8]> for IndexValue {
-    fn from(aml: &[u8]) -> Self {
-        assert!(Self::matches(aml), "aml = {:#x?}", aml);
-        Self(Box::new(aml.into()))
-    }
-}
-
-impl Reader<'_> for IndexValue {
-    fn length(&self) -> usize {
-        self.0.length()
-    }
-
-    fn matches(aml: &[u8]) -> bool {
-        TermArg::matches(aml)
-    }
-}
 
