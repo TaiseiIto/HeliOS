7830ec07c1b6e7ef4b7f882f56beae2ec741acf7
TaiseiIto <taisei1212@outlook.jp>
2024-08-11 10:49:35 +0000

    Analyze AML #15

--- kernel/src/acpi/machine_language/local_obj.rs
+++ kernel/src/acpi/machine_language/local_obj.rs
@@ -1,32 +1,8 @@
-use super::Reader;
-
-const LOCAL_OBJ_MIN: u8 = 0x60;
-const LOCAL_OBJ_MAX: u8 = 0x67;
-
 /// # Local Objects Encoding
 /// ## References
 /// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.6.2 Local Objects Encoding
-#[derive(Debug)]
+#[derive(acpi_machine_language::Reader)]
+#[encoding_value_min = 0x60]
+#[encoding_value_max = 0x67]
 pub struct LocalObj(u8);
-
-impl From<&[u8]> for LocalObj {
-    fn from(aml: &[u8]) -> Self {
-        assert!(Self::matches(aml), "aml = {:#x?}", aml);
-        let local_obj: u8 = *aml.first().unwrap();
-        let local_obj: u8 = local_obj - LOCAL_OBJ_MIN;
-        Self(local_obj)
-    }
-}
-
-impl Reader<'_> for LocalObj {
-    fn length(&self) -> usize {
-        1
-    }
-
-    fn matches(aml: &[u8]) -> bool {
-        aml
-            .first()
-            .is_some_and(|head| (LOCAL_OBJ_MIN..=LOCAL_OBJ_MAX).contains(head))
-    }
-}
 
