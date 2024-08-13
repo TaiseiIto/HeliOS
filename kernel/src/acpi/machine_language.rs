82b8ce0e54c657f6e183b1235cf43dc645f21f16
TaiseiIto <taisei1212@outlook.jp>
2024-08-13 12:43:18 +0000

    Analyze AML #15

--- kernel/src/acpi/machine_language.rs
+++ kernel/src/acpi/machine_language.rs
@@ -1095,6 +1095,7 @@ pub struct Predicate(TermArg);
 /// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.2 Name Objects Encoding
 #[derive(acpi_machine_language::Reader)]
 #[matching_elements = 0]
+#[string]
 pub struct PrefixPath(Vec<Circumflex>);
 
 /// # ReferenceTypeOpcode
