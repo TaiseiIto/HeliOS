97c78b27baae244c92f7807f508694b9815dadcc
TaiseiIto <taisei1212@outlook.jp>
2024-08-13 06:41:44 +0000

    Analyze AML #15

--- kernel/src/acpi/machine_language.rs
+++ kernel/src/acpi/machine_language.rs
@@ -141,7 +141,7 @@ pub enum ComputationalData {
     ByteConst(ByteConst),
     ConstObj(ConstObj),
     DWordConst(DWordConst),
-    String(String),
+    AmlString(AmlString),
     WordConst(WordConst),
 }
 
@@ -1212,7 +1212,7 @@ pub struct StoreOp;
 /// ## References
 /// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.3 Data Objects Encoding
 #[derive(acpi_machine_language::Reader)]
-pub struct String(
+pub struct AmlString(
     StringPrefix,
     AsciiCharList,
     NullChar,
