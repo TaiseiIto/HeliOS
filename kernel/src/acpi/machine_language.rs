afce26811624b59033fb05c9c1c8d8e8e7d8ac01
TaiseiIto <taisei1212@outlook.jp>
2024-08-15 04:19:22 +0000

    Analyze AML #15

--- kernel/src/acpi/machine_language.rs
+++ kernel/src/acpi/machine_language.rs
@@ -840,6 +840,7 @@ impl From<&[u8]> for MethodInvocation {
             | "PIDX"
             | "PNOT"
             | "PPRT"
+            | "PRR0"
             | "PWPR"
             | "PWRR"
             | "P_CS"
