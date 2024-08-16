28d39190d1816c0f7c990cbca853c0c6b2d9a382
TaiseiIto <taisei1212@outlook.jp>
2024-08-16 06:52:17 +0000

    Analyze AML #15

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -309,10 +309,6 @@ fn main(argument: &'static mut Argument<'static>) {
             com2_println!("Local APIC ID = {:#x?}", local_apic_id);
             com2_println!("{}", log);
         });
-    com2_println!("cpuid = {:#x?}", Argument::get().cpuid());
-    com2_println!("efi_system_table = {:#x?}", Argument::get().efi_system_table());
-    com2_println!("rsdp = {:#x?}", Argument::get().efi_system_table().rsdp());
-    unimplemented!();
     // Shutdown.
     Argument::get()
         .efi_system_table()
@@ -320,6 +316,7 @@ fn main(argument: &'static mut Argument<'static>) {
         .xsdt()
         .fadt()
         .shutdown();
+    unimplemented!();
 }
 
 /// # A panic handler of the kernel
