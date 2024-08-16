16921bb39000180873718222e6a4c24e7579876b
TaiseiIto <taisei1212@outlook.jp>
2024-08-16 09:37:57 +0000

    Analyze AML #15

--- kernel/src/acpi/machine_language.rs
+++ kernel/src/acpi/machine_language.rs
@@ -377,6 +377,15 @@ pub struct DefElse(
     TermList,
 );
 
+/// # DefEvent
+/// ## References
+/// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.5.2 Named Objects Encoding
+#[derive(acpi_machine_language::Reader)]
+pub struct DefEvent(
+    EventOp,
+    NameString,
+);
+
 /// # DefExternal
 /// ## References
 /// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.5.2 Named Objects Encoding
@@ -769,6 +778,23 @@ pub struct DualNamePrefix;
 #[derive(acpi_machine_language::Reader)]
 #[encoding_value = 0xa1]
 pub struct ElseOp;
+
+/// # EventOp
+/// ## References
+/// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.5.2 Named Objects Encoding
+#[derive(acpi_machine_language::Reader)]
+#[matching_elements = 2]
+pub struct EventOp(
+    ExtOpPrefix,
+    EventOpSuffix,
+);
+
+/// # EventOpSuffix
+/// ## References
+/// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.5.2 Named Objects Encoding
+#[derive(acpi_machine_language::Reader)]
+#[encoding_value = 0x02]
+pub struct EventOpSuffix;
 
 /// # ExpressionOpcode
 /// ## References
@@ -1324,6 +1350,7 @@ pub struct NamedField(
 pub enum NamedObj {
     CreateDWordField(DefCreateDWordField),
     Device(DefDevice),
+    Event(DefEvent),
     External(DefExternal),
     Field(DefField),
     Method(DefMethod),
