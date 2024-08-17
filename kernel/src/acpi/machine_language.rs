2f1ffd4d3cbf3e95f47c64505285f89afdbc54f7
TaiseiIto <taisei1212@outlook.jp>
2024-08-17 07:51:18 +0000

    Analyze AML #15

--- kernel/src/acpi/machine_language.rs
+++ kernel/src/acpi/machine_language.rs
@@ -300,6 +300,7 @@ pub struct CopyObjectOp;
 #[derive(acpi_machine_language::Reader)]
 pub enum ConstObj {
     OneOp(OneOp),
+    OnesOp(OnesOp),
     ZeroOp(ZeroOp),
 }
 
@@ -2406,6 +2407,13 @@ pub struct ObjectTypeOp;
 #[derive(acpi_machine_language::Reader)]
 #[encoding_value = 0x01]
 pub struct OneOp;
+
+/// # OnesOp
+/// ## References
+/// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.3 Data Objects Encoding
+#[derive(acpi_machine_language::Reader)]
+#[encoding_value = 0xff]
+pub struct OnesOp;
 
 /// # OpRegionOp
 /// ## References
