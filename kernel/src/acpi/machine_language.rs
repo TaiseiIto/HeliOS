09c4ba334a22f4cd8be25e94156948565582dcc0
TaiseiIto <taisei1212@outlook.jp>
2024-08-17 06:23:25 +0000

    Analyze AML #15

--- kernel/src/acpi/machine_language.rs
+++ kernel/src/acpi/machine_language.rs
@@ -327,6 +327,13 @@ pub struct CreateFieldOpSuffix;
 #[encoding_value = 0x8f]
 pub struct CreateQWordFieldOp;
 
+/// # CreateWordFieldOp
+/// ## References
+/// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.5.2 Named Objects Encoding
+#[derive(acpi_machine_language::Reader)]
+#[encoding_value = 0x8b]
+pub struct CreateWordFieldOp;
+
 /// # DWordConst
 /// ## References
 /// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.3 Data Objects Encoding
@@ -568,6 +575,17 @@ pub struct DefCreateField(
 #[derive(acpi_machine_language::Reader)]
 pub struct DefCreateQWordField(
     CreateQWordFieldOp,
+    SourceBuff,
+    ByteIndex,
+    NameString,
+);
+
+/// # DefCreateWordField
+/// ## References
+/// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.5.2 Named Objects Encoding
+#[derive(acpi_machine_language::Reader)]
+pub struct DefCreateWordField(
+    CreateWordFieldOp,
     SourceBuff,
     ByteIndex,
     NameString,
@@ -2024,7 +2042,7 @@ pub enum NamedObj {
     CreateDWordField(DefCreateDWordField),
     CreateField(DefCreateField),
     CreateQWordField(DefCreateQWordField),
-    CreateWordField(DefCreateWrodField),
+    CreateWordField(DefCreateWordField),
     DataRegion(DefDataRegion),
     Device(DefDevice),
     Event(DefEvent),
