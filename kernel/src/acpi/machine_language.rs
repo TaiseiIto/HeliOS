a809cd7d2e7065e1b6817ef370792a5e5b4af0ae
TaiseiIto <taisei1212@outlook.jp>
2024-08-17 03:53:25 +0000

    Analyze AML #15

--- kernel/src/acpi/machine_language.rs
+++ kernel/src/acpi/machine_language.rs
@@ -1626,7 +1626,6 @@ pub struct MethodOp;
 /// ## References
 /// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.5.4 Expression Opcodes Encoding
 #[derive(acpi_machine_language::Reader)]
-#[encoding_value = 0x9e]
 pub struct MidObj(TermArg);
 
 /// # MidOp
