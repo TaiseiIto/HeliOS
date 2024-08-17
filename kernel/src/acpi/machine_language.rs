84a49cd3fae2c4a8611275b1f9a0369368e20cb8
TaiseiIto <taisei1212@outlook.jp>
2024-08-17 06:56:34 +0000

    Analyze AML #15

--- kernel/src/acpi/machine_language.rs
+++ kernel/src/acpi/machine_language.rs
@@ -273,6 +273,13 @@ pub struct CondRefOfOp(
 #[encoding_value = 0x12]
 pub struct CondRefOfOpSuffix;
 
+/// # ContinueOp
+/// ## References
+/// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.5.3 Statement Opcodes Encoding
+#[derive(acpi_machine_language::Reader)]
+#[encoding_value = 0x9f]
+pub struct ContinueOp;
+
 /// # CopyObjectOp
 /// ## References
 /// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.5.4 Expression Opcodes Encoding
@@ -543,6 +550,12 @@ pub struct DefConcatRes(
     [BufData; 2],
     Target,
 );
+
+/// # DefContinue
+/// ## References
+/// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.5.3 Statement Opcodes Encoding
+#[derive(acpi_machine_language::Reader)]
+pub struct DefContinue(ContinueOp);
 
 /// # DefCopyObject
 /// ## References
