8e702b99cbaa8fe651954e20280122eeee17cc78
TaiseiIto <taisei1212@outlook.jp>
2024-08-17 06:16:07 +0000

    Analyze AML #15

--- kernel/src/acpi/machine_language.rs
+++ kernel/src/acpi/machine_language.rs
@@ -303,6 +303,23 @@ pub struct CreateByteFieldOp;
 #[encoding_value = 0x8a]
 pub struct CreateDWordFieldOp;
 
+/// # CreateFieldOp
+/// ## References
+/// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.5.2 Named Objects Encoding
+#[derive(acpi_machine_language::Reader)]
+#[matching_elements = 2]
+pub struct CreateFieldOp(
+    ExtOpPrefix,
+    CreateFieldOpSuffix,
+);
+
+/// # CreateFieldOpSuffix
+/// ## References
+/// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.5.2 Named Objects Encoding
+#[derive(acpi_machine_language::Reader)]
+#[encoding_value = 0x13]
+pub struct CreateFieldOpSuffix;
+
 /// # DWordConst
 /// ## References
 /// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.3 Data Objects Encoding
@@ -523,6 +540,18 @@ pub struct DefCreateDWordField(
     CreateDWordFieldOp,
     SourceBuff,
     ByteIndex,
+    NameString,
+);
+
+/// # DefCreateField
+/// ## References
+/// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.5.2 Named Objects Encoding
+#[derive(acpi_machine_language::Reader)]
+pub struct DefCreateField(
+    CreateFieldOp,
+    SourceBuff,
+    BitIndex,
+    NumBits,
     NameString,
 );
 
@@ -2030,6 +2059,12 @@ pub struct NullChar(char);
 #[derive(acpi_machine_language::Reader)]
 #[encoding_value = 0x00]
 pub struct NullName(char);
+
+/// # NumBits
+/// ## References
+/// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.5.2 Named Objects Encoding
+#[derive(acpi_machine_language::Reader)]
+pub struct NumBits(TermArg);
 
 /// # NumElements
 /// ## References
