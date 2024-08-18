e3a615369fc10c6704068eb175026dab59270def
TaiseiIto <taisei1212@outlook.jp>
2024-08-18 15:17:22 +0900

    Analyze AML #15

--- kernel/src/acpi/machine_language.rs
+++ kernel/src/acpi/machine_language.rs
@@ -353,7 +353,7 @@ pub struct ConnectField(
 /// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.5.2 Named Objects Encoding
 #[derive(acpi_machine_language::Reader)]
 pub enum ConnectFieldEnum {
-    BufferData(BufferData),
+    BufData(BufData),
     NameString(NameString),
 }
 
