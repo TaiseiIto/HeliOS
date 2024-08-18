b2b385481dd27a01216c2cdf4d115d912311f615
TaiseiIto <taisei1212@outlook.jp>
2024-08-18 15:05:49 +0900

    Analyze AML #15

--- kernel/src/acpi/machine_language.rs
+++ kernel/src/acpi/machine_language.rs
@@ -1612,6 +1612,12 @@ pub enum ExpressionOpcode {
 #[encoding_value = 0x5b]
 pub struct ExtOpPrefix;
 
+/// # ExtendedAccessField
+/// ## References
+/// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.5.2 Named Objects Encoding
+#[derive(acpi_machine_language::Reader)]
+pub struct ExtendedAccessField(ByteData);
+
 /// # ExternalOp
 /// ## References
 /// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.5.2 Named Objects Encoding
