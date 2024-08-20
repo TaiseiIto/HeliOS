4cc4920eb907905e8bf9e77676c9eaf8428a0fdf
TaiseiIto <taisei1212@outlook.jp>
2024-08-20 12:19:09 +0000

    Analyze AML #15

--- kernel/acpi_machine_language/src/lib.rs
+++ kernel/acpi_machine_language/src/lib.rs
@@ -903,7 +903,7 @@ fn derive_from_slice_u8(derive_input: &DeriveInput) -> proc_macro2::TokenStream 
                                                         let debug: proc_macro2::TokenStream = if debug {
                                                             quote! {
                                                                 crate::com2_println!("element = {:#x?}", element);
-                                                                crate::com2_println!("remaining_aml = {:02x?}", remaining_aml);
+                                                                crate::com2_println!("remaining_aml = {:02x?}", &remaining_aml[0..core::cmp::min(10, remaining_aml.len())]);
                                                             }
                                                         } else {
                                                             quote! {
