32a5818707b15c706639e20a99fc81070cde85c7
TaiseiIto <taisei1212@outlook.jp>
2024-10-14 12:22:31 +0000

    ACPI generic address space id #15

--- kernel/src/acpi/generic_address.rs
+++ kernel/src/acpi/generic_address.rs
@@ -41,3 +41,46 @@ impl Structure {
     }
 }
 
+/// # Address Space ID
+/// ## References
+/// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 5.2.3.2 Generic Address Structure
+pub enum SpaceId {
+    SystemMemorySpace,
+    SystemIoSpace,
+    PciConfigurationSpace,
+    EmbeddedController,
+    SmBus,
+    SystemCmos,
+    PciBarTarget,
+    Ipmi,
+    GeneralPurposeIo,
+    GenericSerialBus,
+    PlatformCommunicationsChannel,
+    PlatformRuntimeMechanism,
+    Reserved(u8),
+    FunctionalFixedHardware,
+    OemDefined(u8),
+}
+
+impl From<u8> for SpaceId {
+    fn from(space_id: u8) -> Self {
+        match space_id {
+            0x00 => Self::SystemMemorySpace,
+            0x01 => Self::SystemIoSpace,
+            0x02 => Self::PciConfigurationSpace,
+            0x03 => Self::EmbeddedController,
+            0x04 => Self::SmBus,
+            0x05 => Self::SystemCmos,
+            0x06 => Self::PciBarTarget,
+            0x07 => Self::Ipmi,
+            0x08 => Self::GeneralPurposeIo,
+            0x09 => Self::GenericSerialBus,
+            0x0a => Self::PlatformCommunicationsChannel,
+            0x0b => Self::PlatformRuntimeMechanism,
+            space_id @ 0x0c..=0x7e => Self::Reserved(space_id),
+            0x7f => Self::FunctionalFixedHardware,
+            space_id @ 0x80..=0xff => Self::OemDefined(space_id),
+        }
+    }
+}
+
