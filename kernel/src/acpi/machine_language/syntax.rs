f086dd1493effd5b1d93250fe0a1d23f377f25cc
TaiseiIto <taisei1212@outlook.jp>
2024-10-27 10:47:37 +0000

    Evaluate AML DefLAnd #15

--- kernel/src/acpi/machine_language/syntax.rs
+++ kernel/src/acpi/machine_language/syntax.rs
@@ -1561,6 +1561,20 @@ pub struct DefLAnd(
     [Operand; 2],
 );
 
+impl Evaluator for DefLAnd {
+    fn evaluate(&self, stack_frame: &mut interpreter::StackFrame, root: &reference::Node, current: &name::Path) -> Option<interpreter::Value> {
+        let Self(
+            _l_and_op,
+            [left, right],
+        ) = self;
+        let left: Option<interpreter::Value> = left.evaluate(stack_frame, root, current);
+        let right: Option<interpreter::Value> = right.evaluate(stack_frame, root, current);
+        left
+            .zip(right)
+            .map(|(left, right)| interpreter::Value::Bool((&left).into() && (&right).into()))
+    }
+}
+
 /// # DefLEqual
 /// ## References
 /// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.5.4 Expression Opcodes Encoding
@@ -2485,6 +2499,7 @@ impl Evaluator for ExpressionOpcode {
             Self::FromBcd(def_from_bcd) => def_from_bcd.evaluate(stack_frame, root, current),
             Self::Increment(def_increment) => def_increment.evaluate(stack_frame, root, current),
             Self::Index(def_index) => def_index.evaluate(stack_frame, root, current),
+            Self::LAnd(def_l_and) => def_l_and.evaluate(stack_frame, root, current),
             Self::LGreaterEqual(def_l_greater_equal) => def_l_greater_equal.evaluate(stack_frame, root, current),
             Self::MethodInvocation(method_invocation) => method_invocation.evaluate(stack_frame, root, current),
             Self::SizeOf(def_size_of) => def_size_of.evaluate(stack_frame, root, current),
