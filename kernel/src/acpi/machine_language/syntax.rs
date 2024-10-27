c4455e6d2088b22d9811389e2d26b22b215417ea
TaiseiIto <taisei1212@outlook.jp>
2024-10-27 10:58:44 +0000

    Evaluate AML DefLGreater #15

--- kernel/src/acpi/machine_language/syntax.rs
+++ kernel/src/acpi/machine_language/syntax.rs
@@ -1607,6 +1607,20 @@ pub struct DefLGreater(
     [Operand; 2],
 );
 
+impl Evaluator for DefLGreater {
+    fn evaluate(&self, stack_frame: &mut interpreter::StackFrame, root: &reference::Node, current: &name::Path) -> Option<interpreter::Value> {
+        let Self(
+            _l_greater_equal_op,
+            [left, right],
+        ) = self;
+        let left: Option<interpreter::Value> = left.evaluate(stack_frame, root, current);
+        let right: Option<interpreter::Value> = right.evaluate(stack_frame, root, current);
+        left
+            .zip(right)
+            .map(|(left, right)| (left > right).into())
+    }
+}
+
 /// # DefLGreaterEqual
 /// ## References
 /// * [Advanced Configuration and Power Interface (ACPI) Specification](https://uefi.org/sites/default/files/resources/ACPI_Spec_6_5_Aug29.pdf) 20.2.5.4 Expression Opcodes Encoding
@@ -2515,6 +2529,7 @@ impl Evaluator for ExpressionOpcode {
             Self::Index(def_index) => def_index.evaluate(stack_frame, root, current),
             Self::LAnd(def_l_and) => def_l_and.evaluate(stack_frame, root, current),
             Self::LEqual(def_l_equal) => def_l_equal.evaluate(stack_frame, root, current),
+            Self::LGreater(def_l_greater) => def_l_greater.evaluate(stack_frame, root, current),
             Self::LGreaterEqual(def_l_greater_equal) => def_l_greater_equal.evaluate(stack_frame, root, current),
             Self::MethodInvocation(method_invocation) => method_invocation.evaluate(stack_frame, root, current),
             Self::SizeOf(def_size_of) => def_size_of.evaluate(stack_frame, root, current),
