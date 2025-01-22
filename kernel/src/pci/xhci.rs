f83742db37fe3b7fe3e6ec0cb9100ba0002784aa
TaiseiIto <taisei1212@outlook.jp>
2025-01-22 10:16:37 +0000

    Read XHCI host controller capability register #25

--- kernel/src/pci/xhci.rs
+++ kernel/src/pci/xhci.rs
@@ -2,16 +2,39 @@
 //! ## References
 //! * [eXtensible Host Controller Interface for Universal Serial Bus (xHCI)](https://www.intel.com/content/dam/www/public/us/en/documents/technical-specifications/extensible-host-controler-interface-usb-xhci.pdf)
 
-use super::{
-    Function,
-    class,
+use {
+    core::fmt,
+    super::{
+        Function,
+        class,
+    },
 };
 
 pub mod host_controller;
 
-#[derive(Debug)]
 pub struct Registers {
     address: usize,
+}
+
+impl Registers {
+    fn host_controller_capability_register(&self) -> &host_controller::capability::Register {
+        let Self {
+            address,
+        } = self;
+        let host_controller_capability_register: *const host_controller::capability::Register = *address as *const host_controller::capability::Register;
+        unsafe {
+            &*host_controller_capability_register
+        }
+    }
+}
+
+impl fmt::Debug for Registers {
+    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+        formatter
+            .debug_struct("Registers")
+            .field("host_controller_capability_registers", self.host_controller_capability_register())
+            .finish()
+    }
 }
 
 impl TryFrom<&Function> for Registers {
