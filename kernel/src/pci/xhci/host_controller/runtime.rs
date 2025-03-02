f4bbb182342b7f7a210db31359de772da257a2cb
TaiseiIto <taisei1212@outlook.jp>
2025-03-02 20:32:16 +0900

    Read microframe index #25

--- kernel/src/pci/xhci/host_controller/runtime.rs
+++ kernel/src/pci/xhci/host_controller/runtime.rs
@@ -1,9 +1,11 @@
+pub mod mfindex;
+
 /// # Host Controller Runtime Registers
 /// ## References
 /// * [eXtensible Host Controller Interface for Universal Serial Bus (xHCI)](https://www.intel.com/content/dam/www/public/us/en/documents/technical-specifications/extensible-host-controler-interface-usb-xhci.pdf) 5.5 Host Controller Runtime Registers
 #[derive(Debug)]
 #[repr(packed)]
 pub struct Registers {
-    mfindex: u32,
+    mfindex: mfindex::Register,
 }
 
---
+++ kernel/src/pci/xhci/host_controller/runtime/mfindex.rs
@@ -0,0 +1,13 @@
+use bitfield_struct::bitfield;
+
+/// # MFINDEX
+/// ## References
+/// * [eXtensible Host Controller Interface for Universal Serial Bus (xHCI)](https://www.intel.com/content/dam/www/public/us/en/documents/technical-specifications/extensible-host-controler-interface-usb-xhci.pdf) 5.5.1 Microframe Index Register (MFINDEX)
+#[bitfield(u32)]
+pub struct Register {
+    #[bits(14)]
+    microframe_index: u16,
+    #[bits(18)]
+    __: u32
+}
+
