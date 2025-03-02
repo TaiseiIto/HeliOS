77cf8cf49868d8744d4a39eafec6d8d5d6a8dc85
TaiseiIto <taisei1212@outlook.jp>
2025-03-02 21:38:20 +0900

    Read IMAN register #25

--- kernel/src/pci/xhci/host_controller/runtime/interrupter.rs
+++ kernel/src/pci/xhci/host_controller/runtime/interrupter.rs
@@ -1,10 +1,12 @@
+pub mod iman;
+
 /// # Interrupter Register Set
 /// ## References
 /// * [eXtensible Host Controller Interface for Universal Serial Bus (xHCI)](https://www.intel.com/content/dam/www/public/us/en/documents/technical-specifications/extensible-host-controler-interface-usb-xhci.pdf) 5.5.2 Interrupter Register Set
 #[derive(Clone, Copy, Debug)]
 #[repr(packed)]
 pub struct RegisterSet {
-    iman: u32,
+    iman: iman::Register,
     imod: u32,
     erstsz: u32,
     __: u32,
