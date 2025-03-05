fc2e002512126d3faa39118222a0035d2d5639da
TaiseiIto <taisei1212@outlook.jp>
2025-03-05 20:29:27 +0900

    Read MSI capability structures #25

--- kernel/src/pci/msi/capability.rs
+++ kernel/src/pci/msi/capability.rs
@@ -1,13 +1,30 @@
-use super::super::Function;
+use {
+    core::fmt,
+    super::super::Function,
+};
 
 /// # MSI Capability Structure
 /// ## References
 /// * [PCI Local Bus Specification Revision 3.0](https://lekensteyn.nl/files/docs/PCI_SPEV_V3_0.pdf) 6.8.1. MSI Capability Structure
-#[derive(Debug)]
 #[repr(packed)]
 pub struct Structure {
     capability_id: u8,
     next_pointer: u8,
+}
+
+impl fmt::Debug for Structure {
+    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let Self {
+            capability_id,
+            next_pointer,
+        } = self;
+        let capability_id: Id = (*capability_id).into();
+        formatter
+            .debug_struct("Structure")
+            .field("capability_id", &capability_id)
+            .field("next_poitner", next_pointer)
+            .finish()
+    }
 }
 
 pub struct Structures<'a> {
@@ -46,6 +63,56 @@ impl<'a> Iterator for Structures<'a> {
             *next_pointer = structure.next_pointer;
             structure
         })
+    }
+}
+
+/// # Capability IDs
+/// ## References
+/// * [PCI Local Bus Specification Revision 3.0](https://lekensteyn.nl/files/docs/PCI_SPEV_V3_0.pdf) H. Capability IDs
+#[derive(Debug)]
+pub enum Id {
+    Reserved(u8),
+    PciPowerManagementInterface,
+    Agp,
+    Vpd,
+    SlotIdentification,
+    Msi,
+    CompactPciHotSwap,
+    PciX,
+    HyperTransport,
+    VendorSpecific,
+    DebugPort,
+    CompactPciCentralResourceControl,
+    PciHotPlug,
+    PciBridgeSubsystemVendorId,
+    Agp8x,
+    SecureDevice,
+    PciExpress,
+    MsiX,
+}
+
+impl From<u8> for Id {
+    fn from(id: u8) -> Self {
+        match id {
+            0x01 => Self::PciPowerManagementInterface,
+            0x02 => Self::Agp,
+            0x03 => Self::Vpd,
+            0x04 => Self::SlotIdentification,
+            0x05 => Self::Msi,
+            0x06 => Self::CompactPciHotSwap,
+            0x07 => Self::PciX,
+            0x08 => Self::HyperTransport,
+            0x09 => Self::VendorSpecific,
+            0x0a => Self::DebugPort,
+            0x0b => Self::CompactPciCentralResourceControl,
+            0x0c => Self::PciHotPlug,
+            0x0d => Self::PciBridgeSubsystemVendorId,
+            0x0e => Self::Agp8x,
+            0x0f => Self::SecureDevice,
+            0x10 => Self::PciExpress,
+            0x11 => Self::MsiX,
+            id => Self::Reserved(id),
+        }
     }
 }
 
