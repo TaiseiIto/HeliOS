b905e2ba3b18d60b3c4e5ac3ecf170f05090f4d1
TaiseiIto <taisei1212@outlook.jp>
2025-03-10 23:55:37 +0900

    Read MSI tables #25

--- kernel/src/pci/msi/capability/msi_x/pba.rs
+++ kernel/src/pci/msi/capability/msi_x/pba.rs
@@ -1,4 +1,8 @@
-use bitfield_struct::bitfield;
+use {
+    alloc::vec::Vec,
+    bitfield_struct::bitfield,
+    super::super::super::super::base,
+};
 
 /// # PBA Offset/PBA BIR for MSI-X
 /// ## References
@@ -11,8 +15,22 @@ pub struct Register {
     offset: u32,
 }
 
+impl Register {
+    pub fn read(&self, index2address: &base::Index2Address, table_size: usize) -> Vec<PendingBits> {
+        let bir: usize = self.bir() as usize;
+        let offset: u32 = self.offset() << Self::OFFSET_OFFSET;
+        let offset: usize = offset as usize;
+        index2address
+            .get(bir)
+            .unwrap()
+            .offset(offset)
+            .read_vector(table_size)
+    }
+}
+
 /// # Pending Bits
 /// ## References
 /// * [PCI Local Bus Specification Revision 3.0](https://lekensteyn.nl/files/docs/PCI_SPEV_V3_0.pdf) 6.8.2.10. Pending Bits for MSI-X PBA-Entries
+#[derive(Default)]
 pub struct PendingBits(u64);
 
--- kernel/src/pci/msi/capability/msi_x/table.rs
+++ kernel/src/pci/msi/capability/msi_x/table.rs
@@ -1,4 +1,8 @@
-use bitfield_struct::bitfield;
+use {
+    alloc::vec::Vec,
+    bitfield_struct::bitfield,
+    super::super::super::super::base,
+};
 
 /// # Table Offset/Table BIR for MSI-X
 /// ## References
@@ -11,10 +15,23 @@ pub struct Register {
     offset: u32,
 }
 
+impl Register {
+    pub fn read(&self, index2address: &base::Index2Address, table_size: usize) -> Vec<Entry> {
+        let bir: usize = self.bir() as usize;
+        let offset: u32 = self.offset() << Self::OFFSET_OFFSET;
+        let offset: usize = offset as usize;
+        index2address
+            .get(bir)
+            .unwrap()
+            .offset(offset)
+            .read_vector(table_size)
+    }
+}
+
 /// # Table Entry
 /// ## References
 /// * [PCI Local Bus Specification Revision 3.0](https://lekensteyn.nl/files/docs/PCI_SPEV_V3_0.pdf) 6.8.2. Fugure 6-11: MSI-X Table Structure
-#[derive(Debug)]
+#[derive(Debug, Default)]
 #[repr(packed)]
 pub struct Entry {
     msg_addr: u64,
