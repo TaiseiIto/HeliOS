7de82e2b2c49849698dbf36dc8b21582f799f798
TaiseiIto <taisei1212@outlook.jp>
2025-04-20 18:48:17 +0900

    Run HeliOS on VirtualBox

--- .virtualbox/update_vhd.sh
+++ .virtualbox/update_vhd.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-helios=..
-vhd=~/VirtualBox\ VMs/HeliOS/HeliOS.vhd
+vhd=$(vboxmanage showvminfo "HeliOS" | grep vhd | awk -F ':' '{print $2}' | sed s/\"//g)
 nbd=/dev/nbd0
 mnt=mnt
 
-make tree -C $helios
 sudo modprobe nbd max_part=16
 sudo qemu-nbd --format=vpc --connect=$nbd $vhd
 sudo mkfs.vfat -v -c -F 32 $nbd
 mkdir $mnt
 sudo mount $nbd $mnt
-sudo cp -r $helios/HeliOS.mnt/* $mnt
+sudo cp -r ../HeliOS.mnt/* $mnt
 find $mnt
 sudo umount $mnt
 sudo rm -rf $mnt
