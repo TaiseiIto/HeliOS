de8f35fcb29028018fbc7b1077b32980a6a2f0e4
TaiseiIto <taisei1212@outlook.jp>
2025-04-20 23:34:29 +0900

    Calculate media size

--- .vmware/update_vmdk.sh
+++ .vmware/update_vmdk.sh
@@ -5,19 +5,20 @@ virtual_machine_directory=$(dirname $vmx)
 vmdk=$virtual_machine_directory/$(grep vmdk $vmx | awk -F '=' '{print $NF}' | sed s/\"//g | sed 's/ //g')
 vhd=$(echo $vmdk | sed s/vmdk$/vhd/)
 nbd=/dev/nbd0
-destination=destination
-source=$(make mount_directory -C .. -s)
+destination_path=destination
+source_path=$(make mount_directory -C .. -s)
+media_size=$(make media_size -C .. -s)
 
 sudo modprobe nbd max_part=16
-qemu-img create -f vpc $vhd 64M
+qemu-img create -f vpc $vhd $media_size
 sudo qemu-nbd --format=vpc --connect=$nbd $vhd
 sudo mkfs.vfat -v -c -F 32 $nbd
-mkdir $destination
-sudo mount $nbd $destination
-sudo cp -r $source/* $destination
-find $destination
-sudo umount $destination
-sudo rm -rf $destination
+mkdir $destination_path
+sudo mount $nbd $destination_path
+sudo cp -r $source_path/* $destination_path
+find $destination_path
+sudo umount $destination_path
+sudo rm -rf $destination_path
 sudo qemu-nbd --disconnect $nbd
 qemu-img convert -f vpc -O vmdk $vhd $vmdk
 
