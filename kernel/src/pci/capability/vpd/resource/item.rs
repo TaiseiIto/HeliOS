2e6fd1b29896cb1c6bc00b6ceec6dcb629432410
TaiseiIto <taisei1212@outlook.jp>
2025-05-02 16:56:54 +0900

    Read VPD #25

--- kernel/src/pci/capability/vpd/resource/item.rs
+++ kernel/src/pci/capability/vpd/resource/item.rs
@@ -1,7 +1,10 @@
-use alloc::{
-    string::String,
-    vec,
-    vec::Vec,
+use {
+    alloc::{
+        string::String,
+        vec,
+        vec::Vec,
+    },
+    core::str,
 };
 
 /// # VPD Format
@@ -9,7 +12,7 @@ use alloc::{
 /// * [PCI Local Bus Specification Revision 3.0](https://lekensteyn.nl/files/docs/PCI_SPEV_V3_0.pdf) I.1. VPD Format. Figure I-5: VPD Format
 #[derive(Debug)]
 pub struct Format {
-    keyword: String,
+    keyword: Keyword,
     length: u8,
     data: Vec<u8>,
 }
@@ -22,7 +25,9 @@ impl Format {
         match (keyword_low, keyword_high, length) {
             (Some(keyword_low), Some(keyword_high), Some(length)) => {
                 let keyword: Vec<u8> = vec![keyword_low, keyword_high];
-                let keyword: String = String::from_utf8(keyword).unwrap();
+                let keyword: Keyword = str::from_utf8(&keyword)
+                    .unwrap()
+                    .into();
                 let data: Vec<u8> = (0..length)
                     .map(|_| byte_iterator
                         .next()
@@ -59,6 +64,64 @@ impl<T> Iterator for FormatIterator<T> where T: Iterator<Item = u8> {
             byte_iterator,
         } = self;
         Self::Item::from_byte_iterator(byte_iterator)
+    }
+}
+
+/// # VPD Keyword
+/// ## References
+/// * [PCI Local Bus Specification Revision 3.0](https://lekensteyn.nl/files/docs/PCI_SPEV_V3_0.pdf) I.3.1.1. Read-Only Fields
+/// * [PCI Local Bus Specification Revision 3.0](https://lekensteyn.nl/files/docs/PCI_SPEV_V3_0.pdf) I.3.1.2. Read/Write Fields
+#[derive(Debug)]
+pub enum Keyword {
+    Cp,
+    Ec,
+    Fg,
+    Lc,
+    Mn,
+    Pg,
+    Pn,
+    Rv,
+    Rw,
+    Sn,
+    V(char),
+    Ya,
+    Y(char),
+}
+
+impl From<&str> for Keyword {
+    fn from(keyword: &str) -> Self {
+        match keyword {
+            "CP" => Self::Cp,
+            "EC" => Self::Ec,
+            "FG" => Self::Fg,
+            "LC" => Self::Lc,
+            "MN" => Self::Mn,
+            "PG" => Self::Pg,
+            "PN" => Self::Pn,
+            "RV" => Self::Rv,
+            "RW" => Self::Rw,
+            "SN" => Self::Sn,
+            "YA" => Self::Ya,
+            keyword => {
+                let keyword: Vec<char> = keyword
+                    .chars()
+                    .collect();
+                match keyword.as_slice()
+                    .chunks(2)
+                    .next()
+                    .unwrap() {
+                    ['V', x] => {
+                        assert!(matches!(x, '0'..='9' | 'A'..= 'Z'));
+                        Self::V(*x)
+                    },
+                    ['Y', x] => {
+                        assert!(matches!(x, '0'..='9' | 'B'..= 'Z'));
+                        Self::Y(*x)
+                    },
+                    _ => unreachable!(),
+                }
+            },
+        }
     }
 }
 
