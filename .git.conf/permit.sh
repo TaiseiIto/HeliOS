f8806ca4784243438bd00d2e3c76ea6decab67fd
TaiseiIto <taisei1212@outlook.jp>
2025-08-03 20:14:21 +0900

    Get git global email

--- .git.conf/permit.sh
+++ .git.conf/permit.sh
@@ -5,10 +5,11 @@
 # Users don:t have to do it.
 # This script is called from the Makefile in the same directory.
 # Don't execute it directly.
-# Usage: $ ./permit.sh <domain> <developer> <product> <ssh key> <gpg key>
+# Usage: $ ./permit.sh <domain> <developer> <email> <product> <ssh key> <gpg key>
 
 domain=$1
 developer=$2
+email=$3
 product=$3
 ssh_key=$4
 gpg_key=$5
@@ -17,11 +18,8 @@ current_directory=$(pwd)
 work_directory=$(dirname $0)
 cd $work_directory
 
-read -p "Your GitHub user name:" name
-read -p "Your GitHub email address:" email
-
+git config --global user.name $developer
 git config --global user.email $email
-git config --global user.name $name
 git config --global user.signingkey $(head -n1 $gpg_key/signingkey.txt)
 git config --global commit.gpgsign true
 git remote set-url origin git@$domain:$developer/$product.git
