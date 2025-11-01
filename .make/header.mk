2a43658b22f6ea4165d9bf56f7f933bb38d0292b
TaiseiIto <taisei1212@outlook.jp>
2025-11-01 12:38:26 +0900

    Solve comm warning #31

--- .make/header.mk
+++ .make/header.mk
@@ -1,5 +1,5 @@
 SHELL=/bin/bash
 PRODUCT=$(notdir $(abspath .))
-SOURCE_FILES=$(shell comm -23 <(git ls-files --cached --exclude-standard --others -- $1) <(git ls-files --deleted -- $1))
+SOURCE_FILES=$(shell comm -23 <(git ls-files --cached --exclude-standard --others -- $1 | sort) <(git ls-files --deleted -- $1 | sort))
 SUB_TARGET=$(shell make target -C $1 -s)
 
